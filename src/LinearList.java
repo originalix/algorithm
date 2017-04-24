f47fdb85fab323c121d9c36245da6008583f62f3
Lix <xiao.liunit@gmail.com>
2017-04-24 08:00:18 +0800

    implements interface for list

--- src/LinearList.java
+++ src/LinearList.java
@@ -1,2 +1,26 @@
-public class LinearList {
+public class LinearList implements List {
+
+    final int defaultSize = 10;
+    int maxSize;
+    
+
+    public Object FindKth(int K, LinearList L) {
+        return null;
+    }
+
+    public int Find(Object obj, LinearList L) {
+        return 0;
+    }
+
+    public void Insert(Object obj, int i, LinearList L) {
+
+    }
+
+    public void Delete(int i, LinearList L) {
+
+    }
+
+    public int Length(LinearList L) {
+        return 0;
+    }
 }
