9e22091b733ba420eae80257438d78feef405f13
Lix <xiao.liunit@gmail.com>
2017-04-24 10:47:01 +0800

    interface

--- src/LinearList.java
+++ src/LinearList.java
@@ -1,8 +1,29 @@
+import com.sun.org.apache.xml.internal.security.Init;
+
 public class LinearList implements List {
 
     final int defaultSize = 10;
     int maxSize;
-    
+    int size;
+    Object[] listArray;
+
+    public LinearList() {
+        init(defaultSize);
+    }
+
+    public LinearList(int size) {
+        init(size);
+    }
+
+    private void init(int size) {
+        maxSize = size;
+        this.size = size;
+        listArray = new Object[size];
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
     public Object FindKth(int K, LinearList L) {
         return null;
@@ -21,6 +42,6 @@ public class LinearList implements List {
     }
 
     public int Length(LinearList L) {
-        return 0;
+        return this.size;
     }
 }
--- src/List.java
+++ src/List.java
@@ -3,6 +3,7 @@
  * Created by Leon on 17/4/24.
  */
 public interface List {
+    public boolean isEmpyty();
     public Object FindKth(int K, LinearList L);
     public int Find(Object obj, LinearList L);
     public void Insert(Object obj, int i, LinearList L);
