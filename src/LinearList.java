f39f38ae02c594eaddda39879dd665b4fa723210
Lix <xiao.liunit@gmail.com>
2017-05-04 20:22:04 +0800

    listarray

--- src/LinearList.java
+++ src/LinearList.java
@@ -26,18 +26,30 @@ public class LinearList implements List {
             throw new Exception("参数错误");
         }
         for (int j = size - 1; j >= index; j--) {
-            listArray[j+1] = listArray[j];
+            listArray[j + 1] = listArray[j];
         }
         listArray[index] = obj;
         size++;
     }
 
     public void delete(int index, Object obj) throws Exception {
-
+        if (isEmpty()) {
+            throw new Exception("顺序表没有元素，无法删除");
+        }
+        if (index < 0 || index > size - 1) {
+            throw new Exception("参数错误");
+        }
+        for (int j = index; j < size - 1; j++) {
+            listArray[j] = listArray[j + 1];
+        }
+        size--;
     }
 
     public Object get(int index) throws Exception {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new Exception("参数错误");
+        }
+        return listArray[index];
     }
 
     public boolean isEmpty() {
