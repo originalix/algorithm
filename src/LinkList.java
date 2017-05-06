2a12754f8776dccaaddb6a229012e475f6ceab3d
Lix <xiao.liunit@gmail.com>
2017-05-06 15:48:47 +0800

    implements list

--- src/LinkList.java
+++ src/LinkList.java
@@ -41,6 +41,40 @@ class Node {
     }
 }
 
-public class LinkList {
+public class LinkList implements List {
 
+    Node head; //头指针
+    Node current; //当前节点
+    int size; //节点个数
+
+    //初始化一个空链表
+    public LinkList() {
+        this.head = this.current = new Node(null);
+        this.size = 0;
+    }
+
+    @Override
+    public void insert(int index, Object obj) throws Exception {
+
+    }
+
+    @Override
+    public void delete(int index, Object obj) throws Exception {
+
+    }
+
+    @Override
+    public Object get(int index) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
