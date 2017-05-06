ee3ff538487f084a2ba52dd2c48d530a4d97c852
Lix <xiao.liunit@gmail.com>
2017-05-06 15:38:05 +0800

    linknode

--- src/LinkList.java
+++ src/LinkList.java
@@ -2,10 +2,43 @@
  * Created by Leon on 2017/5/5.
  */
 class Node {
-    Object element;
-    Node next;
+    Object element; //数据域
+    Node next; //指针域
 
-    
+    //头结点的构造方法
+    public Node(Node nextval) {
+        this.next = nextval;
+    }
+
+    //非头结点的构造方法
+    public Node(Object obj, Node nextval) {
+        this.element = obj;
+        this.next = nextval;
+    }
+
+    //获得当前节点的指针域
+    public Node getNext() {
+        return this.next;
+    }
+
+    //获得当前节点的数据域
+    public Object getElement() {
+        return this.element;
+    }
+
+    //设置当前节点的指针域
+    public void setNext(Node nextval) {
+        this.next = nextval;
+    }
+
+    //设置当前节点的数据域
+    public void setElement(Object obj) {
+        this.element = obj;
+    }
+
+    public String toString() {
+        return this.element.toString();
+    }
 }
 
 public class LinkList {
