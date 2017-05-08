ffed6da60f8db4de9a1e2e7f5274ced28aa431d5
Lix <xiao.liunit@gmail.com>
2017-05-08 11:40:20 +0800

    array copy

--- src/Stack.java
+++ src/Stack.java
@@ -2,4 +2,33 @@
  * Created by Leon on 2017/5/8.
  */
 public class Stack {
+    private Object[] elementData;
+    private int size = 0;
+    private int defaultSize = 10;
+
+    public Stack() {
+        init(defaultSize);
+    }
+
+    public Stack(int size) {
+        init(size);
+    }
+
+    private void init(int size) {
+        this.size = 0;
+        this.elementData = new  Object[size];
+    }
+
+    public void push(Object obj) {
+
+    }
+
+    private void ensureCapacity() {
+        if (elementData.length == size) {
+            Object[] oldElements = elementData;
+            int newLength = (int)(elementData.length * 1.5);
+            elementData = new Object[newLength];
+            System.arraycopy(oldElements, 0, elementData, 0, size);
+        }
+    }
 }
