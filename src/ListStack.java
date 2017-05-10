24e9cf184eefe10e37b22bb102df6883ba96a640
Lix <xiao.liunit@gmail.com>
2017-05-11 07:33:12 +0800

    constructor

--- src/ListStack.java
+++ src/ListStack.java
@@ -2,4 +2,22 @@
  * Created by Leon on 2017/5/11.
  */
 public class ListStack {
+    Node header;
+    int size;
+    int elementCount;
+
+    public ListStack() {
+        header = null;
+        elementCount = 0;
+        size = 0;
+    }
+
+    public ListStack(int size) {
+        header = null;
+        elementCount = 0;
+        this.size = size;
+    }
+
+    
+
 }
