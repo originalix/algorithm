06c65dc8d6b2cf3b414430811864f377f704da8f
Lix <xiao.liunit@gmail.com>
2017-05-16 07:31:57 +0800

    CircleSequenceQueue property

--- src/CircleSequenceQueue.java
+++ src/CircleSequenceQueue.java
@@ -2,4 +2,11 @@
  * Created by Leon on 2017/5/16.
  */
 public class CircleSequenceQueue {
+    static final int defaultSize = 10; //队列默认长度
+    int front; //队头
+    int rear; //队尾
+    int count; //统计元素个数的技术器
+    int maxSize; //队列最大长度
+    Object[] queue; //队列
+    
 }
