15612e356dc97d15ac0bf7dbb7600ae3ce27ac36
Lix <xiao.liunit@gmail.com>
2017-05-17 07:39:39 +0800

    买票卖票demo

--- src/WindowQueue.java
+++ src/WindowQueue.java
@@ -1,5 +1,22 @@
 /**
  * Created by Leon on 2017/5/17.
  */
+//售票窗口
 public class WindowQueue {
+    // 卖票队列
+    int maxSize = 10;
+    CircleSequenceQueue queue = new CircleSequenceQueue(maxSize);
+    int num = 0; //统计卖票的数量 一天最多卖100张票
+    boolean isAlive = true; //判断是否继续卖票
+
+    //排队买票
+    public synchronized void producter() throws Exception {
+
+    }
+
+    //卖票
+    public synchronized void consumer() throws Exception {
+
+    }
+
 }
