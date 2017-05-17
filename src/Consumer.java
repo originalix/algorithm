2555edac324ff3186d653e12c419e19281742334
Lix <xiao.liunit@gmail.com>
2017-05-17 10:23:08 +0800

    finish consumer class

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,5 +1,23 @@
 /**
  * Created by Leon on 2017/5/17.
  */
+
+//卖票者
 public class Consumer {
+
+    WindowQueue queue;
+
+    public Consumer(WindowQueue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (queue.isAlive) {
+            try {
+                queue.consumer();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
