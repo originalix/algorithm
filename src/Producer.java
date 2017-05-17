2435c9b872220264106c7d7f89776eb3faede425
Lix <xiao.liunit@gmail.com>
2017-05-17 10:21:36 +0800

    finish Producer class

--- src/Producer.java
+++ src/Producer.java
@@ -1,5 +1,22 @@
 /**
  * Created by Leon on 2017/5/17.
  */
+
+//买票者
 public class Producer {
+    WindowQueue queue;
+
+    public Producer(WindowQueue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (queue.num < 100) {
+            try {
+                queue.producter();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
