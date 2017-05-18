8cffe9b193abffc5927009b47ecd9e328174ece6
Lix <xiao.liunit@gmail.com>
2017-05-19 07:11:31 +0800

    init LinkQueue

--- src/LinkQueue.java
+++ src/LinkQueue.java
@@ -2,4 +2,18 @@
  * Created by Leon on 2017/5/19.
  */
 public class LinkQueue {
+    Node front;
+    Node rear;
+    int count;
+
+    public LinkQueue() {
+        init();
+    }
+
+    private void init() {
+        front = rear = null;
+        count = 0;
+    }
+
+
 }
