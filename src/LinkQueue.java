6e26c0c635879b0356aee86edda2fa85a0f71a8a
Lix <xiao.liunit@gmail.com>
2017-05-19 07:14:38 +0800

    init LinkQueue

--- src/LinkQueue.java
+++ src/LinkQueue.java
@@ -1,7 +1,7 @@
 /**
  * Created by Leon on 2017/5/19.
  */
-public class LinkQueue {
+public class LinkQueue implements Queue {
     Node front;
     Node rear;
     int count;
@@ -15,5 +15,22 @@ public class LinkQueue {
         count = 0;
     }
 
+    @Override
+    public void append(Object obj) throws Exception {
+        Node node = new Node(obj, null);
 
+        //如果当前队列不空
+        if (rear != null) {
+            rear.next = node; //队尾节点指向新节点
+        }
+
+        rear = node; //设置队尾节点为新节点
+
+        //说明要插入的节点是队列的第一个节点
+        if (front == null) {
+            front = node;
+        }
+
+        count++;
+    }
 }
