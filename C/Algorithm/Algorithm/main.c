0bb9f9afde117ae93e4b4d32e85dc47dff4d0e76
Lix <xiao.liunit@gmail.com>
2017-05-21 15:49:01 +0800

    Algorithm C

---
+++ C/Algorithm/Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by Lix on 2017/5/21.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
