2bce781b8c6b5a53bddefeb6154e23b2e05ffea4
Lix <xiao.liunit@gmail.com>
2017-05-21 21:12:22 +0800

    树的结构

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -19,3 +19,11 @@ typedef struct PTNode {
     TElemType data;
     int parent;
 }PTNode;
+
+/**
+ 树结构
+ */
+typedef struct {
+    PTNode nodes[MAX_TREE_SIZE]; /* 结点数组 */
+    int r,n; /* 根的位置和结点数 */
+}PTree;
