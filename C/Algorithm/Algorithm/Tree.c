132d359c4514ac5f61984c7252a64245a918fc00
Lix <xiao.liunit@gmail.com>
2017-05-22 08:15:10 +0800

    create T

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -13,60 +13,6 @@
 typedef int TElemType; /* 树节点的数据类型, 目前暂定为整型 */
 typedef int Status; /* Status是函数类型，其值是函数的返回结果 */
 
-/**
- 结点结构
- */
-typedef struct PTNode
-{
-    TElemType data;
-    int parent;
-}PTNode;
-
-/**
- 树结构
- */
-typedef struct
-{
-    PTNode nodes[MAX_TREE_SIZE]; /* 结点数组 */
-    int r,n; /* 根的位置和结点数 */
-}PTree;
-
-/**
- 孩子结点
- */
-typedef struct CTNode
-{
-    int child;
-    struct CTNode *next;
-}*ChildPtr;
-
-/**
- 表头结构
- */
-typedef struct
-{
-    TElemType data;
-    ChildPtr firstchild;
-}CTBox;
-
-/**
- 树结构
- */
-typedef struct
-{
-    CTBox nodes[MAX_TREE_SIZE];
-    int r,n;
-}CTree;
-
-/**
- 树的孩子兄弟表示法结构定义
- */
-typedef struct CSNode
-{
-    TElemType data;
-    struct CSNode *firstchild, *rightsib;
-}CSNode, *CSTree;
-
 
 int main()
 {
