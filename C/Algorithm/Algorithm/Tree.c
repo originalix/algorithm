d5b5f72bb62109fa8af148cc1d552fc130d70d8e
Lix <xiao.liunit@gmail.com>
2017-05-22 16:58:59 +0800

    add comment

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -131,29 +131,31 @@ Status Assign(SqBiTree T, Position e, TElemType value)
 TElemType Parent(SqBiTree T, TElemType e)
 {
     int i;
-    if (T[0] == Nil) {
+    if (T[0] == Nil) { /* 空树 */
         return Nil;
     }
     for (i = 1; i <= MAX_TREE_SIZE - 1; i++) {
-        if (T[i] == e) {
+        if (T[i] == e) { /* 找到e */
             return T[(i+1)/2-1];
         }
     }
-    return Nil;
+    return Nil; /* 未找到 */
 }
 
+/* 初始条件: 二叉树T存在,e是T中某个结点 */
+/* 操作结果: 返回e的左孩子。若e无左孩子,则返回＂空＂ */
 TElemType LeftChild(SqBiTree T, TElemType e)
 {
     int i;
-    if (T[0] == Nil) {
+    if (T[0] == Nil) { /* 空树 */
         return Nil;
     }
     for (i = 0; i <= MAX_TREE_SIZE - 1; i++) {
-        if (T[i] == e) {
+        if (T[i] == e) { /* 找到e */
             return T[i*2+1];
         }
     }
-    return Nil;
+    return Nil; /* 没找到e */
 }
 
 int main()
