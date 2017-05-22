42fc16c005fe3f3d65de7b5462161ae54defa34d
Lix <xiao.liunit@gmail.com>
2017-05-22 08:15:51 +0800

    create T

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -8,8 +8,8 @@
 
 #include <stdio.h>
 
-/* 树的双亲表示法结点结构定义 */
-#define MAX_TREE_SIZE 100
+#define MAX_TREE_SIZE 100 /* 二叉树的最大结点数 */
+
 typedef int TElemType; /* 树节点的数据类型, 目前暂定为整型 */
 typedef int Status; /* Status是函数类型，其值是函数的返回结果 */
 
