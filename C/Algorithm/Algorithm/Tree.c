b179139c6fdb9861539773603a47bffca78c0a95
Lix <xiao.liunit@gmail.com>
2017-05-22 10:14:14 +0800

    InitBiTree

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -20,6 +20,28 @@ typedef int Status; /* Status是函数类型，其值是函数的返回结果 */
 typedef int TElemType; /* 树节点的数据类型, 目前暂定为整型 */
 typedef TElemType SqBiTree[MAX_TREE_SIZE]; /* 0号单元存储根结点 */
 
+typedef struct
+{
+    int lezvel,order; /* 结点的层，本层的序号(按满二叉树计算) */
+}Position;
+
+TElemType Nil = 0; /* 设整型以0为空 */
+
+Status visit(TElemType c)
+{
+    printf("%d\n", c);
+    return OK;
+}
+
+Status InitBiTree(SqBiTree T)
+{
+    int i;
+    for (int i = 0; i < MAX_TREE_SIZE; i++) {
+        T[i] = Nil;
+    }
+    return OK;
+}
+
 int main()
 {
     printf("HELLO WORLD WSX\n");
