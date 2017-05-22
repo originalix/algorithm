4133364a61ad172e6e959ead9aa93c500c30817a
Lix <xiao.liunit@gmail.com>
2017-05-22 08:19:51 +0800

    增加状态值定义

--- C/Algorithm/Algorithm/Tree.c
+++ C/Algorithm/Algorithm/Tree.c
@@ -8,6 +8,12 @@
 
 #include <stdio.h>
 
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 100 /* 存储空间初始分配量 */
 #define MAX_TREE_SIZE 100 /* 二叉树的最大结点数 */
 
 typedef int TElemType; /* 树节点的数据类型, 目前暂定为整型 */
