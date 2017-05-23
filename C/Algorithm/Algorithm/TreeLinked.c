fb721c9c7acae6ae8e9fa6605fda0951a5cecb09
Lix <xiao.liunit@gmail.com>
2017-05-24 07:40:02 +0800

    构建线索二叉树

---
+++ C/Algorithm/Algorithm/TreeLinked.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 100 /* 存储空间初始分配量 */
+
+typedef int Status; /* Status是函数类型，其值是函数的返回结果 */
