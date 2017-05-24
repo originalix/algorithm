df1518621abfd815a4c3b7cd8ab6c5dda51537e8
Lix <xiao.liunit@gmail.com>
2017-05-24 20:56:02 +0800

    fix bug

--- C/Algorithm/Algorithm/main.c
+++ C/Algorithm/Algorithm/main.c
@@ -16,17 +16,18 @@ long factorial(int n)
     long result;
 
     if (n == 0 || n == 1) {
-        result =;
+        result = 1;
     } else {
         printf("递归调用函数\n");
         result = factorial(n - 1) * n;
-        printf("递归返回 %d\n", result);
+        printf("递归返回 %ld\n", result);
     }
     return result;
 }
 
 int main()
 {
+
     printf("hello world\n");
     return 0;
 }
