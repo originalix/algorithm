e6549cc01778fb5b986da9f9e6ba8015960d2938
Lix <xiao.liunit@gmail.com>
2017-06-12 13:19:31 +0800

    add comment

--- C/Algorithm/Algorithm/Graph1.c
+++ C/Algorithm/Algorithm/Graph1.c
@@ -16,17 +16,17 @@ void CreateMGraph(MGraph *G)
 {
     int i, j, k, w;
     printf("输入顶点数和边数:\n");
-    scanf("%d, %d", &G->numVertexes, &G->numEdges);
-    for (i = 0; i < G->numVertexes; i++)
+    scanf("%d, %d", &G->numVertexes, &G->numEdges); /* 输入顶点和边数 */
+    for (i = 0; i < G->numVertexes; i++)  /* 读入顶点信息，建立顶点表 */
         scanf(&G->vexs[i]);
     for (i = 0; i < G->numVertexes; i++)
         for (j = 0; j < G->numVertexes; j++)
-            G->arc[i][j] = INFINITY;
-    for (k = 0; k < G->numVertexes; k++)
+            G->arc[i][j] = INFINITY;  /* 邻接矩阵初始化 */
+    for (k = 0; k < G->numVertexes; k++)  /* 读入numEdges条边，建立邻接矩阵 */
     {
         printf("输入边(vi, vj)上的下标i, 下标j和权w: \n");
-        scanf("%d, %d, %d", &i, &j, &w);
+        scanf("%d, %d, %d", &i, &j, &w);  /* 输入边(vi, vj)上的权 w */
         G->arc[i][j] = w;
-        G->arc[j][i] = G->arc[i][j];
+        G->arc[j][i] = G->arc[i][j];  /* 因为是无向图，矩阵对称 */
     }
 }
