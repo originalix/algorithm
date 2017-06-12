d116a133324ee0ea38792c37c4f84d0ad81842cb
Lix <xiao.liunit@gmail.com>
2017-06-12 11:38:43 +0800

    邻接矩阵定义

--- C/Algorithm/Algorithm/Graph1.c
+++ C/Algorithm/Algorithm/Graph1.c
@@ -1,1 +1,13 @@
 /* 邻接矩阵的创建 */
+
+typedef char VertexType;
+typedef int EdgeType;
+#define MAXVEX 100
+#define INFINITY 65535
+
+typedef struct
+{
+    VertexType vexs[MAXVEX];
+    EdgeType arc[MAXVEX][MAXVEX];
+    int numVertexes, numEdges;
+}MGraph;
