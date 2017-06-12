a3e7fa548ea544f43f44c02a76d8719129e665ad
Lix <xiao.liunit@gmail.com>
2017-06-12 12:30:30 +0800

    Create MGraph

--- C/Algorithm/Algorithm/Graph1.c
+++ C/Algorithm/Algorithm/Graph1.c
@@ -11,3 +11,22 @@ typedef struct
     EdgeType arc[MAXVEX][MAXVEX];  /* 邻接矩阵，可看作边表 */
     int numVertexes, numEdges;  /* 图中当前的顶点数和边数 */
 }MGraph;
+
+void CreateMGraph(MGraph *G)
+{
+    int i, j, k, w;
+    printf("输入顶点数和边数:\n");
+    scanf("%d, %d", &G->numVertexes, &G->numEdges);
+    for (i = 0; i < G->numVertexes; i++)
+        scanf(&G->vexs[i]);
+    for (i = 0; i < G->numVertexes; i++)
+        for (j = 0; j < G->numVertexes; j++)
+            G->arc[i][j] = INFINITY;
+    for (k = 0; k < G->numVertexes; k++)
+    {
+        printf("输入边(vi, vj)上的下标i, 下标j和权w: \n");
+        scanf("%d, %d, %d", &i, &j, &w);
+        G->arc[i][j] = w;
+        G->arc[j][i] = G->arc[i][j];
+    }
+}
