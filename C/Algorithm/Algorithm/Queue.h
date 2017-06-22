936ad4de324ac9ec90094c50056738a1af497235
Lix <xiao.liunit@gmail.com>
2017-06-22 17:28:41 +0800

    [Queue.h]: 插入 删除定义

--- C/Algorithm/Algorithm/Queue.h
+++ C/Algorithm/Algorithm/Queue.h
@@ -1,5 +1,6 @@
 #ifndef __QUEUE_H_
 #define __QUEUE_H_
+
 typedef struct queue
 {
     int *pBase;
@@ -12,4 +13,7 @@ void CreateQueue(PQUEUE Q, int maxsize);
 void TraverseQueue(PQUEUE Q);
 bool FullQueue(PQUEUE Q);
 bool EmptyQueue(PQUEUE Q);
+bool Enqueue(PQUEUE Q, int val);
+bool Dequeue(PQUEUE Q, int *val);
+
 #endif
