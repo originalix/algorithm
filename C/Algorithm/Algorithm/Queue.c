72080c1d17f75de1b7663a9966f85c20a1085cb0
Lix <xiao.liunit@gmail.com>
2017-06-22 17:34:19 +0800

    [Queue.h]: 创建队列

--- C/Algorithm/Algorithm/Queue.c
+++ C/Algorithm/Algorithm/Queue.c
@@ -3,3 +3,15 @@
 #include "malloc.h"
 #include "queue.h"
 
+void CreateQueue(PQUEUE Q, int maxsize)
+{
+    Q->pBase = (int *)malloc(sizeof(int)*maxsize);
+    if (NULL == Q->pBase)
+    {
+        printf("Memory allocation failure\n");
+        exit(-1);
+    }
+    Q->front = 0;
+    Q->rear = 0;
+    Q->maxsize = maxsize;
+}
