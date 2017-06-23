069ee719176f02af95b2a2b0f1303be3f8302f24
Lix <xiao.liunit@gmail.com>
2017-06-23 11:24:26 +0800

    [Queue.c]: 调整返回类型

--- C/Algorithm/Algorithm/Queue.c
+++ C/Algorithm/Algorithm/Queue.c
@@ -10,104 +10,105 @@
  * @param Q       PQUEUE
  * @param maxsize int
  */
-// void CreateQueue(PQUEUE Q, int maxsize)
-// {
-//     Q->pBase = (int *)malloc(sizeof(int)*maxsize);
-//     if (NULL == Q->pBase)
-//     {
-//         printf("Memory allocation failure\n");
-//         exit(-1);
-//     }
-//     Q->front = 0;
-//     Q->rear = 0;
-//     Q->maxsize = maxsize;
-// }
+void CreateQueue(PQUEUE Q, int maxsize)
+{
+    Q->pBase = (int *)malloc(sizeof(int)*maxsize);
+    if (NULL == Q->pBase)
+    {
+        printf("Memory allocation failure\n");
+        exit(-1);
+    }
+    Q->front = 0;
+    Q->rear = 0;
+    Q->maxsize = maxsize;
+}
 
 /**
  * Traverse Queue Print the stack element.
  *
  * @param Q PQUEUE
  */
-// void TraverseQueue(PQUEUE Q)
-// {
-//     int i = Q->front;
-//     printf("队中的元素是:\n");
-//     while(i % Q->maxsize != Q->rear)
-//     {
-//         printf("%d", Q->pBase[i]);
-//         i++;
-//     }
-//     printf("\n");
-// }
+void TraverseQueue(PQUEUE Q)
+{
+    int i = Q->front;
+    printf("队中的元素是:\n");
+    while(i % Q->maxsize != Q->rear)
+    {
+        printf("%d", Q->pBase[i]);
+        i++;
+    }
+    printf("\n");
+}
 
-// /**
-//  * Check the queue size was full.
-//  *
-//  * @param  Q PQUEUE
-//  * @return   bool
-//  */
-// bool FullQueue(PQUEUE Q)
-// {
-//     if (Q->front == (Q->rear + 1) % Q->maxsize)
-//         return true;
-//     else
-//         return false;
-// }
+/**
+ * Check the queue size was full.
+ *
+ * @param  Q PQUEUE
+ * @return   bool
+ */
+Status FullQueue(PQUEUE Q)
+{
+    if (Q->front == (Q->rear + 1) % Q->maxsize)
+        return FALSE;
+    else
+        return TRUE;
+}
 
-// /**
-//  * Return queue is empty.
-//  *
-//  * @param  Q PQUEUE
-//  * @return   bool
-//  */
-// bool EmptyQueue(PQUEUE Q)
-// {
-//     if (Q->front == Q->rear)
-//         return true;
-//     else
-//         return false;
-// }
+/**
+ * Return queue is empty.
+ *
+ * @param  Q PQUEUE
+ * @return   bool
+ */
+Status EmptyQueue(PQUEUE Q)
+{
+    if (Q->front == Q->rear)
+        return TRUE;
+    else
+        return FALSE;
+}
 
-// /**
-//  * insert element to Queue.
-//  *
-//  * @param  Q   PQUEUE
-//  * @param  val element
-//  * @return     bool
-//  */
-// bool Enqueue(PQUEUE Q, int val)
-// {
-//     if (FullQueue)
-//     {
-//         return false;
-//     } else {
-//         Q->pBase[Q->rear] = val;
-//         Q->rear = (Q->rear + 1) % Q->maxsize;
-//         return true;
-//     }
-// }
+/**
+ * insert element to Queue.
+ *
+ * @param  Q   PQUEUE
+ * @param  val element
+ * @return     bool
+ */
+Status Enqueue(PQUEUE Q, int val)
+{
+    if (FullQueue(Q))
+    {
+        return FALSE;
+    } else {
+        Q->pBase[Q->rear] = val;
+        Q->rear = (Q->rear + 1) % Q->maxsize;
+        return TRUE;
+    }
+}
 
-// /**
-//  * Delete element in queue.
-//  *
-//  * @param  Q   PQUEUE
-//  * @param  val ELEMENT
-//  * @return     BOOL
-//  */
-// bool Dequeue(PQUEUE Q, int *val)
-// {
-//     if (EmptyQueue)
-//     {
-//         return FALSE;
-//     } else {
-//         *val = Q->pBase[Q->front];
-//         Q->front = (Q->front + 1) % Q->maxsize;
-//         return TRUE;
-//     }
-// }
+/**
+ * Delete element in queue.
+ *
+ * @param  Q   PQUEUE
+ * @param  val ELEMENT
+ * @return     BOOL
+ */
+Status Dequeue(PQUEUE Q, int *val)
+{
+    if (EmptyQueue(Q))
+    {
+        return FALSE;
+    } else {
+        *val = Q->pBase[Q->front];
+        Q->front = (Q->front + 1) % Q->maxsize;
+        return TRUE;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     printf("hello world\n");
+    printf("status %d\n", OK);
     return 0;
 }
