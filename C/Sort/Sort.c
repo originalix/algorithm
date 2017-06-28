3fa2110d4c7cb856fad386fe5bb168b1a044c100
Lix <xiao.liunit@gmail.com>
2017-06-28 16:37:34 +0800

    [Sort]: 简易冒泡

--- C/Sort/Sort.c
+++ C/Sort/Sort.c
@@ -9,3 +9,17 @@ void swap(SqList *L, int i, int j)
     L->r[j] = temp;
 }
 
+void bubbleSort0(SqList *L)
+{
+    int i, j;
+    for (i = 1; i < L->length; i++)
+    {
+        for (j = i+1; j <= L->length; j++)
+        {
+            if (L->r[i] > L->r[j])
+            {
+                swap(L, i, j);
+            }
+        }
+    }
+}
