49b3b749b8730113c1c11808bae906ef92a8df52
Lix <xiao.liunit@gmail.com>
2017-07-04 07:56:38 +0800

    [MaxSubsequenceSum] 实现核心算法

--- C/MaxSubsequenceSum/MaxSubsequenceSum.c
+++ C/MaxSubsequenceSum/MaxSubsequenceSum.c
@@ -1,7 +1,36 @@
-static int MaxSubSum(const int A[], int left, int right)
+static int MaxSubSum(const int A[], int Left, int Right)
 {
     int MaxLeftSum, MaxRightSum;
     int MaxLeftBorderSum, MaxRightBorderSum;
     int LeftBorderSum, RightBorderSum;
     int Center, i;
+
+    if (Left == Right)
+        if (A[Left] > 0)
+            return A[Left];
+        else
+            return 0;
+
+    Center = (Left + Right) / 2;
+    MaxLeftSum = MaxSubSum(A, Left, Center);
+    MaxRightSum = MaxSubSum(A, Center + 1, Right);
+
+    MaxLeftBorderSum = 0; LeftBorderSum = 0;
+    for (i = center; i >= Left; i--)
+    {
+        LeftBorderSum += A[i];
+        if (LeftBorderSum > MaxLeftBorderSum)
+            MaxLeftBorderSum = LeftBorderSum;
+    }
+
+    MaxRightBorderSum = 0; RightBorderSum = 0;
+    for (i = center + 1; i <= Right; i++)
+    {
+        RightBorderSum += A[i];
+        if (RightBorderSum > MaxRightBorderSum)
+            MaxRightBorderSum = RightBorderSum;
+    }
+
+    /* 尚未完成max3 函数 */
+    return 0;
 }
