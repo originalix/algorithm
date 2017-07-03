50280d34a0c9a2155619062cb876de3bc96b79d3
Lix <xiao.liunit@gmail.com>
2017-07-04 07:50:09 +0800

    [MaxSubsequenceSum] property

---
+++ C/MaxSubsequenceSum/MaxSubsequenceSum.c
@@ -0,0 +1,7 @@
+static int MaxSubSum(const int A[], int left, int right)
+{
+    int MaxLeftSum, MaxRightSum;
+    int MaxLeftBorderSum, MaxRightBorderSum;
+    int LeftBorderSum, RightBorderSum;
+    int Center, i;
+}
