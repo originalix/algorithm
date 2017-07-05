6e021967746e192eeb10830011b541c0106de984
Lix <xiao.liunit@gmail.com>
2017-07-06 07:49:00 +0800

    幂运算

--- C/MaxSubsequenceSum/BinarySearch.c
+++ C/MaxSubsequenceSum/BinarySearch.c
@@ -36,6 +36,18 @@ unsigned int Gcd(unsigned int M, unsigned int N)
     return M;
 }
 
+long int Pow(long int X, unsigned in N)
+{
+    if (N == 0)
+        return 1;
+    if (N == 1)
+        return X;
+    if (isEven(N))
+        return Pow(X * X, N / 2);
+    else
+        return Pow(X * X, N / 2) * X;
+}
+
 int main(int argc, char const *argv[])
 {
     printf("Hello WSX\n");
