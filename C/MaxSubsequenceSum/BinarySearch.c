3327edeb3df54de9e7fccf74700228737c725c50
Lix <xiao.liunit@gmail.com>
2017-07-05 08:09:55 +0800

    [BinarySearch.c]  BinarySearch core code

--- C/MaxSubsequenceSum/BinarySearch.c
+++ C/MaxSubsequenceSum/BinarySearch.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NotFound -1;
+
+int BinarySearch(const int A[], int X, int N)
+{
+    int Low, Mid, High;
+
+    Low = 0; High = N - 1;
+    while(Low <= High)
+    {
+        Mid = (Low + High) / 2;
+        if (A[Mid] < X)
+            Low = Mid + 1;
+        else
+        if (A[Mid] > X)
+            High = Mid - 1;
+        else
+            return Mid;
+    }
+    return NotFound;
+}
+
 int main(int argc, char const *argv[])
 {
     printf("Hello WSX\n");
