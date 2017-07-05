8940d710e9b90d303bba91a460688e00ecd7b66d
Lix <xiao.liunit@gmail.com>
2017-07-05 16:36:29 +0800

    [Sort.c] 希尔排序

--- C/Sort/Sort.c
+++ C/Sort/Sort.c
@@ -106,6 +106,29 @@ void ShellSort(SqList *L)
     }
 }
 
+void ShellSort(SqList *L)
+{
+    int i, j, k = 0;
+    int increment = L->length;
+    do
+    {
+        increment = increment / 3 + 1;
+        for (i = increment + 1; i <= L->length; i++)
+        {
+            if (L->r[i] < L->r[i - increment])
+            {
+                L->r[0] = L->r[i];
+                for (j = i - increment; j > 0 && L->r[0] < L->r[j]; j-= increment)
+                    L->r[j + increment] = L->r[j];
+                L->r[j + increment] = L->r[0];
+            }
+        }
+        printf(" 第%d趟排序结果: \n", ++k);
+        print(*L);
+    }
+    while(increment > 1);
+}
+
 int main(int argc, char const *argv[])
 {
     printf("Hello WSX\n");
