70ea3a9d5098d1fcf333f6bd72e34f71844facc3
Lix <xiao.liunit@gmail.com>
2017-07-07 11:44:42 +0800

    change time func

--- C/BookTest.c
+++ C/BookTest.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include "time.h"
 
-int timeTest()
+void timeTest()
 {
-    long i = 1000000000000L;
+    long i = 100000000000L;
 
     clock_t start_time, end_time;
     double duration_time;
     start_time = clock();
 
-    while(i--);
+    for(int j = 0; j < i; j++)
+    {
+
+    }
 
     end_time = clock();
-    duration_time = (double)(end_time - start_time) / CLOCK_PER_SEC;
+    duration_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
     printf("duration : %lf seconds\n", duration_time);
 }
 
 int main(int argc, char const *argv[])
 {
     printf("Hello WSX\n");
+    timeTest();
     return 0;
 }
