46c5c9ad82cd413002f9b6729ea289ef8007498c
Lix <xiao.liunit@gmail.com>
2017-07-12 08:35:58 +0800

    求素数

--- C/PrimeNumber.c
+++ C/PrimeNumber.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void PrimeSimple()
+int PrimeSimple(int num)
 {
+    int a = 0;
 
+    if (num < 1)
+    {
+        printf("小于1不是素数! \n");
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            a++;
+        }
+    }
+
+    if (a > 0)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 int main(int argc, char const *argv[])
 {
+    int result = PrimeSimple(3);
+    printf("素数结果 : %d\n", result);
     printf("Hello wsx\n");
     return 0;
 }
