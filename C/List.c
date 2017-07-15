3b9e801c47d5ec125e2fdff067a19cc240e4ad2b
leon <xiao.liunit@gmail.com>
2017-07-15 17:34:31 +0800

    初始化链表

--- C/List.c
+++ C/List.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include "List.h"
 
+struct Node
+{
+    ElementType Element;
+    Position Next;
+};
+
+void init_list(List L)
+{
+    L = NULL;
+    printf("初始化链表成功\n");
+}
+
 int main(int argc, char const *argv[])
 {
     printf("Hello wsx\n");
