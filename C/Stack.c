6246879170d9c5d9124f918ade5d562a85b97756
Lix <xiao.liunit@gmail.com>
2017-07-17 07:56:00 +0800

    create stack function

--- C/Stack.c
+++ C/Stack.c
@@ -10,6 +10,32 @@
 
 typedef int Status;
 
+struct Node
+{
+    ElementType Element;
+    PtrToNode Next;
+};
+
+int IsEmpty(Stack S)
+{
+    if (S->Next == NULL)
+        return TRUE;
+    else
+        return ERROR;
+}
+
+Stack CreateStack()
+{
+    Stack S;
+
+    S = malloc(sizeof(struct Node));
+    if (S == NULL)
+        printf("Out of space!\n");
+    S->Next == NULL;
+    MakeEmpty(S);
+    return S;
+}
+
 int main(int argc, char const *argv[])
 {
     printf("Hello wsx\n");
