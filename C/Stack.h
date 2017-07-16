ce9f6775031d451f139a2fa35981f033e24cd7ab
Lix <xiao.liunit@gmail.com>
2017-07-17 07:49:14 +0800

    Stack header interface

--- C/Stack.h
+++ C/Stack.h
@@ -1,3 +1,16 @@
 #ifndef _Stack_H
 
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode Stack;
+typedef int ElementType;
+
+int IsEmpty(Stack S);
+Stack CreateStack(void);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType X, Stack S);
+ElementType Top(Stack S);
+void Pop(Stack S);
+
 #endif
