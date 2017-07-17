d9c6ac78a1149ed60b151c23c472c5598fbf1e5a
Lix <xiao.liunit@gmail.com>
2017-07-18 07:49:30 +0800

    stack array header

--- C/StackArray.h
+++ C/StackArray.h
@@ -1,3 +1,17 @@
 #ifndef _StackArray_h
 
-#endif
+struct StackRecord;
+typedef struct StackRecord *Stack;
+typedef int ElementType;
+
+int IsEmpty(Stack S);
+int IsFull(Stack S);
+Stack CreateStack(int MaxElements);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType X, Stack S);
+ElementType Top(Stack S);
+void Pop(Stack S);
+ElementType TopAndPop(Stack S);
+
+#endif /* _Stack_h*/
