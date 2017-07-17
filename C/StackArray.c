9d4c8367fe311d825d5f5b7cc948bea2e6e0a16f
Lix <xiao.liunit@gmail.com>
2017-07-18 07:53:14 +0800

    implemenation file for stack array

--- C/StackArray.c
+++ C/StackArray.c
@@ -8,7 +8,18 @@
 #define TRUE 1
 #define FALSE 0
 
+#define EmptyTOS (-1)
+#define MinStackSize (5)
+
 typedef int Status;
+
+struct StackRecord
+{
+    int Capacity;
+    int TopOfStack;
+    ElementType *Array;
+};
+
 
 int main(int argc, char const *argv[])
 {
