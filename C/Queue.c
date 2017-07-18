61b619699bff18971ccb009299401a89550d9725
Lix <xiao.liunit@gmail.com>
2017-07-18 21:24:41 +0800

    create queue

--- C/Queue.c
+++ C/Queue.c
@@ -15,24 +15,43 @@ struct QueueRecord
     int Capacity;
     int Front;
     int Rear;
-    int MaxSize;
+    int Size;
     ElementType *Array;
-}
+};
 
 Status IsEmpty(Queue Q)
 {
-    if (Q->size == 0)
+    if (Q->Size == 0)
         return TRUE;
     else
         return FALSE;
 }
 
-Statuc IsFull(Queue Q)
+Status IsFull(Queue Q)
 {
     if ((Q->Rear + 1) % Q->Capacity == Q->Front)
         return TRUE;
     else
         return FALSE;
+}
+
+void MakeEmpty(Queue Q)
+{
+    Q->Size = 0;
+    Q->Front = 0;
+    Q->Rear = 0;
+}
+
+Queue CreateQueue(int MaxElements)
+{
+    Queue Q;
+    Q = ( Queue )malloc(sizeof( struct QueueRecord ));
+    if (Q == NULL)
+        printf("Out of space\n");
+    Q->Capacity = MaxElements;
+    MakeEmpty(Q);
+
+    return Q;
 }
 
 int main(int argc, char const *argv[])
