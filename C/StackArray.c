d70a5dc13c0c253aac103d88573b8fdadf0ed2f6
Lix <xiao.liunit@gmail.com>
2017-07-18 08:15:29 +0800

    test main

--- C/StackArray.c
+++ C/StackArray.c
@@ -10,6 +10,7 @@
 
 #define EmptyTOS (-1)
 #define MinStackSize (5)
+#define MaxElements (10)
 
 typedef int Status;
 
@@ -20,22 +21,20 @@ struct StackRecord
     ElementType *Array;
 };
 
-Stack CreateStack(int MaxElements)
+Stack CreateStack()
 {
     Stack S;
 
     if (MaxElements < MinStackSize)
         printf("Stack size is too small\n");
-        exit(-1);
 
     S = malloc( sizeof( struct StackRecord ) );
     if (S == NULL)
         printf("Out of space !!!\n");
-        exit(-1);
+
     S->Array = malloc( sizeof( ElementType ) * MaxElements );
     if (S->Array == NULL)
         printf("Out of space !!!\n");
-        exit(-1);
     S->Capacity = MaxElements;
     MakeEmpty(S);
 
@@ -52,6 +51,14 @@ void DisposeStack(Stack S)
 Status IsEmpty(Stack S)
 {
     if (S->TopOfStack == EmptyTOS)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+Status IsFull(Stack S)
+{
+    if (S->TopOfStack == MaxElements)
         return TRUE;
     else
         return FALSE;
@@ -97,5 +104,15 @@ ElementType TopAndPop(Stack S)
 int main(int argc, char const *argv[])
 {
     printf("Hello wsx\n");
+    Stack S;
+    if(!(S = CreateStack()))
+        printf("Create stack failed.\n");
+    else
+        printf("Create stack success\n");
+
+    if (IsEmpty(S))
+        printf("Stack is empty.\n");
+    else
+        printf("Stack is not empty.\n");
     return 0;
 }
--- C/StackArray.h
+++ C/StackArray.h
@@ -6,7 +6,7 @@ typedef int ElementType;
 
 int IsEmpty(Stack S);
 int IsFull(Stack S);
-Stack CreateStack(int MaxElements);
+Stack CreateStack();
 void DisposeStack(Stack S);
 void MakeEmpty(Stack S);
 void Push(ElementType X, Stack S);
