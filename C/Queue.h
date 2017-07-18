73c3da333c6545e8e542683f420d49b87637bdc4
Lix <xiao.liunit@gmail.com>
2017-07-18 20:45:02 +0800

    queue's operation

--- C/Queue.h
+++ C/Queue.h
@@ -1,3 +1,16 @@
 #ifndef __Queue_h
 
+struct QueueRecord;
+typedef struct QueueRecord *Queue;
+
+int IsEmpty(Queue Q);
+int IsFull(Queue Q);
+Queue CreateQueue();
+void DisposeQueue(Queue Q);
+void MakeEmpty(Queue Q);
+void Enqueue(ElementType X, Queue Q);
+ElementType Front(Queue Q);
+void Dequeue(Queue Q);
+ElementType FrontAndDequeue(Queue Q);
+
 #endif
