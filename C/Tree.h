9ff2c2139b19e7734cb04cd33224cc7449a50c57
Lix <xiao.liunit@gmail.com>
2017-07-21 07:59:30 +0800

    tree interface

--- C/Tree.h
+++ C/Tree.h
@@ -4,5 +4,12 @@ struct TreeNode;
 typedef struct TreeNode *Position;
 typedef struct TreeNode *SearchTree;
 
+SearchTree MakeEmpty( SearchTree T );
+Position Find( ElementType X, SearchTree T );
+Position FindMin( SearchTree T );
+Position FindMax( SearchTree T );
+SearchTree Insert( ElementType X, SearchTree T );
+SearchTree Delete( ElementType X, SearchTree T );
+ElementType Retrieve( Position P );
 
 #endif
