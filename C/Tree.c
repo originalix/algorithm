bd9c013f5be95d75c61966cc85b13326af9ff821
Lix <xiao.liunit@gmail.com>
2017-07-21 08:05:59 +0800

    Make Empty Tree function

--- C/Tree.c
+++ C/Tree.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Queue.h"
+#include "Tree.h"
 
 #define OK 1
 #define ERROR 0
@@ -9,3 +9,27 @@
 #define FALSE 0
 
 typedef int Status;
+
+struct TreeNode
+{
+    ElementType Element;
+    SearchTree Left;
+    SearchTree Right;
+};
+
+SearchTree MakeEmpty(SearchTree T)
+{
+    if (T != NULL)
+    {
+        MakeEmpty(T->Left);
+        MakeEmpty(T->Right);
+        free(T);
+    }
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello wsx\n");
+    return 0;
+}
