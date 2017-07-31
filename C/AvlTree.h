9d7971627b93935572999ee40fa61214e9bf8ffd
Lix <xiao.liunit@gmail.com>
2017-07-31 08:10:00 +0800

    define AvlTree operation

---
+++ C/AvlTree.h
@@ -0,0 +1,15 @@
+#ifndef_AvlTree_H
+
+struct AvlNode;
+typedef struct AvlNode *Position;
+typedef struct AvlNode *AvlTree;
+
+AvlTree MakeEmpty( AvlTree T );
+Position Find( ElementType X, AvlTree T );
+Position FindMin( AvlTree T );
+Position FindMax( AvlTree T );
+AvlTree Insert( ElementType X, AvlTree T );
+AvlTree Delete( ElementType X, AvlTree T );
+ElementType Retrieve( Position P );
+
+#endif /* _AvlTree_H */
