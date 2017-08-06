b45f26e478a6759271e9bd06f7c4acbad4a97e79
Lix <xiao.liunit@gmail.com>
2017-08-06 09:53:09 +0800

    hash table initialize

--- C/HashSep.c
+++ C/HashSep.c
@@ -8,6 +8,8 @@
 #define TRUE 1
 #define FALSE 0
 
+#define MinTableSize 5
+
 typedef int Status;
 
 struct ListNode
@@ -23,6 +25,37 @@ struct HashTbl
     int TableSize;
     List *TheLists;
 };
+
+HashTable InitializeTable(int TableSize)
+{
+    HashTable H;
+    int i;
+
+    if (TableSize < MinTableSize)
+    {
+        printf("Table Size too small\n");
+        return NULL;
+    }
+
+    H = malloc(sizeof(struct HashTbl));
+    if (H == NULL)
+        printf("Out of space!!!\n");
+    H->TableSize = NextPrime(TableSize);
+
+    H->TheLists = malloc(sizeof(List) * H->TableSize);
+    if (H->TheLists == NULL)
+        printf("Out of space\n");
+    for(i = 0; i < H->TableSize; i++)
+    {
+        H->TableSize[i] = malloc(sizeof(struct ListNode));
+        if (H->TheLists[i] == NULL)
+            printf("Out of Space!!!\n");
+        else
+            H->TheLists[i]->Next = NULL;
+    }
+
+    return H;
+}
 
 int main(int argc, char const *argv[])
 {
