6449fc10a458f89d802d88e9235d00f20d63812a
Lix <xiao.liunit@gmail.com>
2017-08-08 07:49:33 +0800

    define hash function

--- C/HashQuad.h
+++ C/HashQuad.h
@@ -1,5 +1,16 @@
 #ifndef _HashQuad_H
 
+typedef unsigned int Index;
+typedef Index Position;
 
+struct HashTbl;
+typedef struct HashTbl *HashTable;
+
+HashTable InitializeTable(int TableSize);
+void DestroyTable(HashTable H);
+Position Find(ElementType Key, HashTable H);
+void Insert(ElementType Key, HashTable H);
+ElementType Retrieve(Position P, HashTable H);
+HashTable Rehash(HashTable H);
 
 #endif
