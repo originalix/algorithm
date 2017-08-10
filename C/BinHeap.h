97416d6d8759f184ae9ca7dff3953667fc9c11dd
Lix <xiao.liunit@gmail.com>
2017-08-10 08:07:27 +0800

    binheap header

--- C/BinHeap.h
+++ C/BinHeap.h
@@ -1,5 +1,15 @@
 #ifndef _BinHeap_H
 
+struct HeapStruct;
+typedef struct HeapStruct *PriorityQueue;
 
+PriorityQueue Initialize( int MaxElements );
+void Destroy( PriorityQueue H );
+void MakeEmpty( PriorityQueue H );
+void Insert( ElementType X, PriorityQueue H );
+ElementType DeleteMin( PriorityQueue H );
+ElementType FindMin( PriorityQueue H );
+int IsEmpty( PriorityQueue H );
+int IsFull( PriorityQueue H );
 
 #endif
