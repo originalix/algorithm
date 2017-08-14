42ccc4b0c7c1d9a4a96db10bca8c3fd0798b28d6
Lix <xiao.liunit@gmail.com>
2017-08-14 08:20:08 +0800

    测试冒泡排序

--- C/test.c
+++ C/test.c
@@ -2,6 +2,29 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world!\n");
+
     return 0;
 }
+
+
+// public class Solution {
+//     /**
+//      * @param A an integer array
+//      * @return void
+//      */
+//     public void sortIntegers(int[] arr) {
+//         // Write your code here
+//         int size;
+//         size = arr.length;
+//         int temp;
+//         for (int i = 0; i < size - 1; i++) {
+//             for (int j = 0; j < size - 1 - i; j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     temp = arr[j];
+//                     arr[ j ] = arr[j + 1];
+//                     arr[j + 1] = arr[j];
+//                 }
+//             }
+//         }
+//     }
+// }
