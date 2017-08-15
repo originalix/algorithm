0ba934bfa5a97b9bb255c2e3b56dd3d14bc9141f
Lix <xiao.liunit@gmail.com>
2017-08-15 20:28:26 +0800

    merge sort

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -3,6 +3,35 @@
 
 using namespace std;
 
+/* 将arr[l...mid]和arr[mid+1...r]两部分进行归并 */
+template<typename T>
+void __merge(T arr[], int l, int mid, int r) {
+    T aux[r-l+1];
+    for (int i = l; i <= r; i++) {
+        aux[i-l] = arr[i];
+    }
+
+    int i = l, j = mid + 1;
+    for (int k = l; k <= r; k++) {
+        if (i > mid) {
+            arr[k] = aux[j-l];
+            j++;
+        }
+        else if (j > r) {
+            arr[k] = aux[i-l];
+            i++;
+        }
+        else if (aux[i-l] < aux[j-l]) {
+            arr[k] = aux[i-l];
+            i++;
+        }
+        else {
+            arr[k] = aux[j-l];
+            j++;
+        }
+    }
+}
+
 template<typename T>
 void __mergeSort(T arr[], int l, int r) {
     if (l >= r)
