4ae3d11088944fd773d617fa6b3e1331c3242c22
Lix <xiao.liunit@gmail.com>
2017-08-15 12:43:46 +0800

    改进插入排序

--- C++/InsertionSort.cpp
+++ C++/InsertionSort.cpp
@@ -12,7 +12,7 @@ void insertionSort(T arr[], int n) {
         // 寻找元素arr[i]合适的插入位置
         T e = arr[i];
         int j;
-        for (int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+        for (j = i; j > 0 && arr[j-1] > e; j--) {
             arr[j] = arr[j-1];
         }
         arr[j] = e;
@@ -20,12 +20,13 @@ void insertionSort(T arr[], int n) {
 }
 
 int main() {
-    int n = 100000;
+    int n = 10000;
     int *arr = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr, n);
 
     SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
     SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
+
 
     delete[] arr;
     cout << "Hello Wsx" << endl;
