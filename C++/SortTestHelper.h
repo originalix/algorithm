2fcd9eae14ac13c59053f81139f3d6c55ddd3b1b
Lix <xiao.liunit@gmail.com>
2017-08-15 11:49:09 +0800

    isSorted function

--- C++/SortTestHelper.h
+++ C++/SortTestHelper.h
@@ -30,6 +30,16 @@ namespace SortTestHelper {
     }
 
     template<typename T>
+    bool isSorted(T arr[], int n) {
+
+        for (int i = 0; i < n; i++)
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        return true;
+    }
+
+    template<typename T>
     void testSort(string sortName, void(*sort)(T[], int), T arr[], int n) {
 
         clock_t startTime = clock();
