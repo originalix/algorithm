0cbe2992520e6f3597f62d39b6d1000246c7c1da
Lix <xiao.liunit@gmail.com>
2017-08-15 10:26:53 +0800

    student operator < and <<

--- C++/Student.h
+++ C++/Student.h
@@ -1,4 +1,23 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student
+{
+    string name;
+    int score;
+
+    bool operator<(const Student& otherStudent) {
+        return score < otherStudent.score;
+    }
+
+    friend ostream& operator<<(ostream &os, Student &student) {
+        os << "Student: " << student.name << " " << student.score << endl;
+        return os;
+    }
+};
 #endif // STRUDENT_H
