84335bce28897561abd36d55b807733c64be859b
Lix <xiao.liunit@gmail.com>
2017-08-15 20:21:08 +0800

    mergeSort

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+template<typename T>
+void __mergeSort(T arr[], int l, int r) {
+    if (l >= r)
+        return;
+    int mid = (l + r) / 2;
+    __mergeSort(arr, l, mid);
+    __mergeSort(arr, mid + 1, r);
+    __merge(arr, l, mid, r);
+}
+
+template<typename T>
+void mergeSort(T arr[], int n) {
+    __mergeSort(arr, 0, n-1);
+}
+
 int main() {
     int n = 100;
     int *arr = SortTestHelper::generateRandomArray(n, 0, n);
