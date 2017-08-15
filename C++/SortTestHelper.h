f8a0a07cc23c5dd8938f1dc48178abfeadbe2fc8
Lix <xiao.liunit@gmail.com>
2017-08-15 08:12:06 +0800

    generate random array

--- C++/SortTestHelper.h
+++ C++/SortTestHelper.h
@@ -2,11 +2,23 @@
 #define SORTTESTHELPER_H
 
 #include <iostream>
+#include <ctime>
+#include <cassert>
 
 using namespace std;
 
 namespace SortTestHelper {
+    /* 生成有n个元素的随机数组，每个元素的随机范围为[RangeL, RangeR] */
+    int* generateRandomArray(int n, int RangeL, int RangeR) {
 
+        assert(RangeL <= RangeR);
+
+        int *arr = new int[n];
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+            arr[i] = rand() % (RangeR - RangeL + 1) + RangeR;
+        return arr;
+    }
 }
 
 #endif //SORTTESTHELPER_H
