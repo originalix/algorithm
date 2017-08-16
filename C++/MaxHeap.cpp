05aa963af6b2c33563f0340dcafc440ae38ae25a
Lix <xiao.liunit@gmail.com>
2017-08-17 07:59:06 +0800

    maxheap insert operate

--- C++/MaxHeap.cpp
+++ C++/MaxHeap.cpp
@@ -14,6 +14,13 @@ private:
     Item *data;
     int count;
 
+    void shiftUp( int k ) {
+        while(k > 1 && data[k/2] < data[k]) {
+            swap( data[k/2], data[k] );
+            k /= 2;
+        }
+    }
+
 public:
     MaxHeap(int capacity) {
         data = new Item[capacity + 1];
@@ -23,10 +30,25 @@ public:
     ~MaxHeap() {
         delete[] data;
     }
+
+    int size() {
+        return count;
+    }
+
+    bool isEmpty() {
+        return count == 0;
+    }
+
+    void insert(Item item) {
+        data[ count + 1 ] = item;
+        count++;
+        shiftUp( count );
+    }
 };
 
 int main() {
-
+    MaxHeap<int> maxheap = MaxHeap<int>(100);
+    cout << maxheap.size() << endl;
     cout << "Hello wsx" << endl;
     return 0;
 }
