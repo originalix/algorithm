364409288aa87f078ea0a994523de75510c5412d
Lix <xiao.liunit@gmail.com>
2017-08-16 21:47:42 +0800

    delete lintcode project

--- C++/SelectionSort.h
+++ C++/SelectionSort.h
@@ -9,7 +9,7 @@ template <typename T>
 void selectionSort(T arr[], int n) {
     for (int i = 0; i < n; i++) {
 
-        // 寻找[i, n]区间里最小值
+        // 寻找[i, n)区间里最小值
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex])
