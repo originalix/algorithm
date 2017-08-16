54ed085fd5f7d48293697612f1620328f811d450
Lix <xiao.liunit@gmail.com>
2017-08-17 07:49:09 +0800

    initialize MaxHeap

--- C++/MaxHeap.cpp
+++ C++/MaxHeap.cpp
@@ -7,6 +7,24 @@
 
 using namespace std;
 
+template <typename Item>
+class MaxHeap {
+
+private:
+    Item *data;
+    int count;
+
+public:
+    MaxHeap(int capacity) {
+        data = new Item[capacity + 1];
+        count = 0;
+    }
+
+    ~MaxHeap() {
+        delete[] data;
+    }
+};
+
 int main() {
 
     cout << "Hello wsx" << endl;
