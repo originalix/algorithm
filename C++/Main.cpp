0c5d257d4fd3b94331914b98519dc5f38c7f55b7
Lix <xiao.liunit@gmail.com>
2017-08-17 21:46:41 +0800

    create .gitignore

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -20,6 +20,14 @@ void heapSort1(T arr[], int n) {
     }
 }
 
+template<typename T>
+void heapSort2(T arr[], int n) {
+    MaxHeap<T> maxheap = MaxHeap<T>(arr, n);
+    for (int i = n - 1; i >= 0; i--) {
+        arr[i] = maxheap.extractMax();
+    }
+}
+
 int main() {
     int n = 1000000;
     cout << "Test for Random Array, size = " << n << ", Random range [0, " << n << "]" << endl;
@@ -28,6 +36,7 @@ int main() {
     int *arr3 = SortTestHelper::copyIntArray(arr2, n);
     int *arr4 = SortTestHelper::copyIntArray(arr3, n);
     int *arr5 = SortTestHelper::copyIntArray(arr4, n);
+    int *arr6 = SortTestHelper::copyIntArray(arr5, n);
 
     // SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
     SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
@@ -36,6 +45,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr1, n);
     SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr5, n);
     SortTestHelper::testSort("heapSort1", heapSort1, arr4, n);
+    SortTestHelper::testSort("heapSort2", heapSort2, arr6, n);
 
     delete[] arr1;
     delete[] arr2;
@@ -53,6 +63,7 @@ int main() {
     arr3 = SortTestHelper::copyIntArray(arr2, n);
     arr4 = SortTestHelper::copyIntArray(arr3, n);
     arr5 = SortTestHelper::copyIntArray(arr4, n);
+    arr6 = SortTestHelper::copyIntArray(arr5, n);
 
     // SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
     SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
@@ -61,6 +72,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr1, n);
     SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr5, n);
     SortTestHelper::testSort("heapSort1", heapSort1, arr4, n);
+    SortTestHelper::testSort("heapSort2", heapSort2, arr6, n);
 
     delete[] arr1;
     delete[] arr2;
