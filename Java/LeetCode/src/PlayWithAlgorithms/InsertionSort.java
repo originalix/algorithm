ec72d695e8725c3a803f91685b82d1904b25591d
Lix <xiao.liunit@gmail.com>
2017-08-17 17:29:22 +0800

    finish insertion sort

--- Java/LeetCode/src/PlayWithAlgorithms/InsertionSort.java
+++ Java/LeetCode/src/PlayWithAlgorithms/InsertionSort.java
@@ -4,4 +4,17 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/17.
  */
 public class InsertionSort {
+
+    private InsertionSort() {}
+
+    public void sort(Comparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            Comparable e = arr[i];
+            int j = i;
+            for ( ; j > 0 && (arr[j-1].compareTo(e) > 0); j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = e;
+        }
+    }
 }
