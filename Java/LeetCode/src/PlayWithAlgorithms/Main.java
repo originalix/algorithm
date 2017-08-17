bfb3bf8d0fab226b26d4a91ae9158f780cb9c9bf
Lix <xiao.liunit@gmail.com>
2017-08-17 20:18:52 +0800

    merge test success

--- Java/LeetCode/src/PlayWithAlgorithms/Main.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Main.java
@@ -7,28 +7,29 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        int N = 100000;
+        int N = 50000;
 
         // 测试1：一般测试
         System.out.println("Test for random array, size = " + N + ", random range [ 0, " + N +" ]");
         Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
         Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
-        SortTestHelper.testSort("PlayWithAlgorithms.SelectionSort", arr1);
-        SortTestHelper.testSort("PlayWithAlgorithms.InsertionSort", arr2);
-
-        System.out.println();
-
-        // 测试2: 有序性更强的测试
-        System.out.println("Test for more ordered random array, size = " + N + ", random range [ 0, 3 ]");
-
-        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
-        arr2 = Arrays.copyOf(arr1, arr1.length);
-
-        SortTestHelper.testSort("PlayWithAlgorithms.SelectionSort", arr1);
-        SortTestHelper.testSort("PlayWithAlgorithms.InsertionSort", arr2);
-
-        System.out.println();
+//        SortTestHelper.testSort("PlayWithAlgorithms.SelectionSort", arr1);
+//        SortTestHelper.testSort("PlayWithAlgorithms.InsertionSort", arr2);
+          SortTestHelper.testSort("PlayWithAlgorithms.MergeSort", arr1);
+//
+//        System.out.println();
+//
+//        // 测试2: 有序性更强的测试
+//        System.out.println("Test for more ordered random array, size = " + N + ", random range [ 0, 3 ]");
+//
+//        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
+//        arr2 = Arrays.copyOf(arr1, arr1.length);
+//
+//        SortTestHelper.testSort("PlayWithAlgorithms.SelectionSort", arr1);
+//        SortTestHelper.testSort("PlayWithAlgorithms.InsertionSort", arr2);
+//
+//        System.out.println();
 
 
     }
