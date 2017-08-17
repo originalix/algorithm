6b08cc6a9c799966091b74b51f5f13be2cb69ef7
Lix <xiao.liunit@gmail.com>
2017-08-17 16:09:52 +0800

    finish #283

--- Java/LeetCode/src/LeetCode/MoveZeroes_283.java
+++ Java/LeetCode/src/LeetCode/MoveZeroes_283.java
@@ -5,6 +5,22 @@ package LeetCode;
  */
 public class MoveZeroes_283 {
 
+    public void moveZeros(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, k);
+                k++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
     public static void main(String[] args) {
 
     }
