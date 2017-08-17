0244d98f3e4f16eaa646218cb023da77465b0095
Lix <xiao.liunit@gmail.com>
2017-08-17 08:22:44 +0800

    finish extractMax and shiftDown for maxshep

--- C++/MaxHeap.cpp
+++ C++/MaxHeap.cpp
@@ -16,9 +16,21 @@ private:
     int capacity;
 
     void shiftUp( int k ) {
-        while(k > 1 && data[k/2] < data[k]) {
+        while (k > 1 && data[k/2] < data[k]) {
             swap( data[k/2], data[k] );
             k /= 2;
+        }
+    }
+
+    void shiftDown( int k ) {
+        while (2*k < count) {
+            int j = 2 * k;
+            if (j + 1 <= count && data[j] < data[j+1])
+                j += 1;
+            if (data[k] > data[j])
+                break;
+            swap( data[k], data[j] );
+            k = j;
         }
     }
 
@@ -46,6 +58,19 @@ public:
         data[ count + 1 ] = item;
         count++;
         shiftUp( count );
+    }
+
+    Item extractMax() {
+        assert(count > 0);
+
+        Item ret = data[1];
+
+        swap( data[1], data[count] );
+        count--;
+
+        shiftDown( 1 );
+
+        return ret;
     }
 
 public:
