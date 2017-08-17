2d9fb40692fbace96938c9ec076ba6a3181c87f6
Lix <xiao.liunit@gmail.com>
2017-08-17 17:15:36 +0800

    在算法中使用泛型

--- Java/LeetCode/src/PlayWithAlgorithms/SelectionSort.java
+++ Java/LeetCode/src/PlayWithAlgorithms/SelectionSort.java
@@ -8,19 +8,19 @@ public class SelectionSort {
     // 我们的算法不允许产生任何实例
     private SelectionSort() {}
 
-    public static void sort(int[] arr) {
+    public static void sort(Comparable[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex])
+                if (arr[j].compareTo( arr[minIndex] ) < 0)
                     minIndex = j;
             }
             swap(arr, i, minIndex);
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int t = arr[i];
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
