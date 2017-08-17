87121f0f393789fde7b9ac2ee9007d2e63e5db54
Lix <xiao.liunit@gmail.com>
2017-08-17 21:04:02 +0800

    quick sort three ways

--- C++/QuickSort.h
+++ C++/QuickSort.h
@@ -103,3 +103,43 @@ template <typename T>
 void quickSort2(T arr[], int n) {
     __quickSort2(arr, 0, n-1);
 }
+
+/*************  三路快排  ******************/
+template <typename T>
+void __quickSort3Ways(T arr[], int l, int r) {
+    if (r - l <= 15) {
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    swap(arr[l], arr[rand()%(r-l+1)+l]);
+    T v = arr[l];
+
+    int lt = l;  // arr[l+1...lt] < v
+    int gt = r + 1;  //arr[gt...r] > v
+    int i = l + 1; // arr[lt+1...i) == v
+
+    while(i < gt) {
+         if (arr[i] < v) { // arr[i] < v
+            swap(arr[i], arr[lt+1]);
+            lt++;
+            i++;
+         }
+         else if (arr[i] > v) { // arr[i] > v
+            swap(arr[i], arr[gt-1]);
+            gt--;
+         }
+         else { // arr[i] == v
+            i++;
+         }
+    }
+
+    swap(arr[l], arr[lt]);
+    __quickSort3Ways(arr, l, lt-1);
+    __quickSort3Ways(arr, gt, r);
+}
+
+template <typename T>
+void quickSort3Ways(T arr[], int n) {
+    __quickSort3Ways(arr, 0, n-1);
+}
