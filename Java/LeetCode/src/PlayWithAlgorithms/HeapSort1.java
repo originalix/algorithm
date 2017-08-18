a5d96df3601f80b1a4ae5e72db0c1c0e00ae3ee5
Lix <xiao.liunit@gmail.com>
2017-08-18 12:11:41 +0800

    write heapSort sort funciton

--- Java/LeetCode/src/PlayWithAlgorithms/HeapSort1.java
+++ Java/LeetCode/src/PlayWithAlgorithms/HeapSort1.java
@@ -4,4 +4,18 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/18.
  */
 public class HeapSort1 {
+
+    private HeapSort1() {}
+
+    public static void sort(Comparable[] arr) {
+        int n = arr.length;
+        MaxHeap<Comparable> maxHeap = new MaxHeap<Comparable>(n);
+        for (int i = 0; i < n; i++) {
+            maxHeap.insert(arr[i]);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            arr[i] = maxHeap.extractMax();
+        }
+    }
 }
