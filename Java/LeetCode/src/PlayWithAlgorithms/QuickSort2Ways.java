3b55e4ec5a2aedf117d38f6fd7854383f8cc4570
Lix <xiao.liunit@gmail.com>
2017-08-18 14:46:31 +0800

    finish quick sort 2 ways

--- Java/LeetCode/src/PlayWithAlgorithms/QuickSort2Ways.java
+++ Java/LeetCode/src/PlayWithAlgorithms/QuickSort2Ways.java
@@ -7,20 +7,49 @@ public class QuickSort2Ways {
 
     private QuickSort2Ways() {}
     public static void sort(Comparable[] arr) {
-
+        sort(arr, 0, arr.length-1);
     }
 
     private static void sort(Comparable[] arr, int l, int r) {
+        if (r - l <= 15) {
+            InsertionSort.sort(arr, l, r);
+            return;
+        }
 
+        int p = partition(arr, l, r);
+        sort(arr, l, p-1);
+        sort(arr, p+1, r);
     }
 
     private static int partition(Comparable[] arr, int l, int r) {
+        swap(arr, l, (int) Math.random() * (r - l + 1) + l);
+        Comparable v = arr[l];
 
+        int i = l + 1, j = r;
+        while (true) {
+            while (i <= r && arr[i].compareTo( v ) < 0) {
+                i++;
+            }
+            while (j >= l + 1 && arr[j].compareTo( v ) > 0) {
+                j++;
+            }
+            if (i > j) break;
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+
+        swap(arr, l, j);
+        return j;
     }
 
     private static void swap(Object[] arr, int i, int j) {
         Object t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
