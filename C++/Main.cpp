355ee0aef6e6aaa3ce86d3dcc2953b2422eef75a
Lix <xiao.liunit@gmail.com>
2017-08-19 16:13:52 +0800

    improve index heap

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -14,12 +14,15 @@ class MaxHeap {
 private:
     Item *data;
     int *indexes;
+    int *reverse;
     int count;
     int capacity;
 
     void shiftUp( int k ) {
         while (k > 1 && data[ indexes[k/2] ] < data[ indexes[k] ]) {
             swap( indexes[k/2], indexes[k] );
+            reverse[indexes[k / 2]] = k / 2;
+            reverse[indexes[k]] = k;
             k /= 2;
         }
     }
@@ -33,6 +36,8 @@ private:
             if (data[ indexes[k] ] > data[ indexes[j] ])
                 break;
             swap( indexes[k], indexes[j] );
+            reverse[ indexes[k] ] = k;
+            reverse[ indexes[j] ] = j;
             k = j;
         }
     }
@@ -42,6 +47,10 @@ public:
     MaxHeap(int capacity) {
         data = new Item[capacity + 1];
         indexes = new int[capacity + 1];
+        reverse = new int[capacity + 1];
+        for (int i = 0; i <= capacity; i++) {
+            reverse[i] = 0;
+        }
         count = 0;
         this->capacity = capacity;
     }
@@ -59,6 +68,7 @@ public:
     ~MaxHeap() {
         delete[] data;
         delete[] indexes;
+        delete[] reverse;
     }
 
     // 返回堆中的元素个数
@@ -76,9 +86,12 @@ public:
     void insert(int i, Item item) {
         assert(count + 1 <= capacity);
         assert(i + 1 >= 1 && i + 1 <= capacity);
+
         i += 1;
         data[i] = item;
         indexes[count+1] = i;
+        reverse[i] = count + 1;
+
         count++;
         shiftUp( count );
     }
@@ -90,6 +103,8 @@ public:
         Item ret = data[ indexes[1] ];
 
         swap( indexes[1], indexes[count] );
+        reverse[ indexes[1] ] = 1;
+        reverse[ indexes[count] ] = 0;
         count--;
 
         shiftDown( 1 );
@@ -101,28 +116,43 @@ public:
         assert( count > 0 );
         int ret = indexes[1] - 1;
         swap( indexes[1], indexes[count] );
+        reverse[ indexes[1] ] = 1;
+        reverse[ indexes[count] ] = 0;
         count--;
         shiftDown(1);
         return ret;
     }
 
+    bool contain( int i ) {
+        assert( i + 1 >= 1 && i + 1 <= capacity);
+        return reverse[i+1] != 0;
+    }
+
     Item getItem( int i ) {
+        assert(contain[i]);
         return data[i + 1];
     }
 
     void change( int i, Item newItem ) {
+
+        assert(contain[i]);
+
         i += 1;
         data[i] = newItem;
 
         // 找到indexes[j] = i, j 表示 data[i]在堆中的位置
         // 之后shiftUp(j), 再shiftDown(j)
-        for ( int j = 1; j <= count; j++ ) {
-            if (indexes[j] == i) {
-                shiftUp(j);
-                shiftDown(j);
-                return;
-            }
-        }
+        // for ( int j = 1; j <= count; j++ ) {
+        //     if (indexes[j] == i) {
+        //         shiftUp(j);
+        //         shiftDown(j);
+        //         return;
+        //     }
+        // }
+
+        int j = reverse[i];
+        shiftUp(j);
+        shiftDown(j);
     }
 
     // 获取最大堆中的堆顶元素
