3495bb55bb2f02f2bfa94dbd29725530179af352
Lix <xiao.liunit@gmail.com>
2017-08-19 15:13:49 +0800

    finish heap sort by java

--- Java/LeetCode/src/PlayWithAlgorithms/HeapSort.java
+++ Java/LeetCode/src/PlayWithAlgorithms/HeapSort.java
@@ -5,5 +5,39 @@ package PlayWithAlgorithms;
  */
 public class HeapSort {
     private HeapSort() {}
-    
+
+    public static void sort(Comparable[] arr) {
+        int n = arr.length;
+        for ( int i = (n-1) / 2; i >= 0; i-- ) {
+            shiftDown(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            shiftDown(arr, i, 0);
+        }
+    }
+
+    private static void shiftDown(Comparable[] arr, int n, int k) {
+        while ( 2*k-1 < n) {
+            int j = 2 * k - 1;
+            if (j < j + 1 && arr[j].compareTo( arr[j+1] ) < 0) {
+                j += 1;
+            }
+
+            if (arr[k].compareTo( arr[j] ) > 0) {
+                break;
+            }
+
+            swap(arr, j, k);
+
+            k = j;
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[j] = arr[i];
+        arr[i] = t;
+    }
 }
