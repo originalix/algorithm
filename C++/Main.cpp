6d4a49b9d9c5598fe580844c40189bc2d4c2ac20
Lix <xiao.liunit@gmail.com>
2017-08-21 07:38:36 +0800

    binary search

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -2,5 +2,21 @@
 
 using namespace std;
 
+template<typename T>
+void binarySearch(T arr[], int n, int target) {
+    int l = 0, r = n - 1;
+    while(l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            r = mid - 1;
+        if (arr[mid] > target)
+            l = mid + 1;
+    }
+
+    return -1;
+}
+
 int main() {
 }
