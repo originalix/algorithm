ea8ba1956d34df4ee3b0bcaeacb71bbfc44ae71d
Lix <xiao.liunit@gmail.com>
2017-08-20 14:53:30 +0800

    finish insert function for Index-Max-Heap in java

--- Java/LeetCode/src/PlayWithAlgorithms/IndexMaxHeap.java
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMaxHeap.java
@@ -22,13 +22,46 @@ public class IndexMaxHeap<Item extends Comparable> {
         this.capacity = capacity;
     }
 
-    int size() {
+    public int size() {
         return count;
     }
 
-    boolean isEmpty() {
+    public boolean isEmpty() {
         return count == 0;
     }
-    
 
+    public void insert(int i, Item item) {
+        assert (count + 1 <= capacity);
+        assert (i + 1 >= 1 && i + 1 <= capacity);
+
+        assert ( !contain(i) );
+
+        i += 1;
+        data[i] = item;
+        indexes[count + 1] = i;
+        reverse[i] = count + 1;
+
+        count++;
+        shiftUp( count );
+    }
+
+    private boolean contain(int i) {
+        assert (i + 1 >= 1 && i + 1 <= capacity);
+        return reverse[i+1] != 0;
+    }
+
+    private void shiftUp(int k) {
+        while (k > 1 && data[ indexes[k / 2] ].compareTo( data[ indexes[k] ] ) < 0) {
+            swap(indexes, k / 2, k);
+            reverse[ indexes[k / 2] ] = k / 2;
+            reverse[ indexes[k] ] = k;
+            k /= 2;
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
 }
