6b2f1022c41ba0403c59af689bb07753372f3eb3
Lix <xiao.liunit@gmail.com>
2017-08-20 14:44:34 +0800

    construction Index-Max-Heap

--- Java/LeetCode/src/PlayWithAlgorithms/IndexMaxHeap.java
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMaxHeap.java
@@ -3,9 +3,32 @@ package PlayWithAlgorithms;
 /**
  * Created by Lix on 2017/8/20.
  */
-public class IndexMaxHeap {
+public class IndexMaxHeap<Item extends Comparable> {
 
-    private IndexMaxHeap() {}
+    protected Item[] data;
+    protected int[] indexes;
+    protected int[] reverse;
+    protected int count;
+    protected int capacity;
 
+    public IndexMaxHeap(int capacity) {
+        data = (Item[]) new Comparable[capacity + 1];
+        indexes = new int[capacity + 1];
+        reverse = new int[capacity + 1];
+        for (int i = 0; i <= capacity; i++) {
+            reverse[i] = 0;
+        }
+        count = 0;
+        this.capacity = capacity;
+    }
+
+    int size() {
+        return count;
+    }
+
+    boolean isEmpty() {
+        return count == 0;
+    }
     
+
 }
