1aba978c83aaca5ec3314ed05945bee32d01955e
Lix <xiao.liunit@gmail.com>
2017-08-20 15:11:10 +0800

    finish Index-max-heap-sort

---
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMaxHeapSort.java
@@ -0,0 +1,22 @@
+package PlayWithAlgorithms;
+
+/**
+ * Created by Lix on 2017/8/20.
+ */
+public class IndexMaxHeapSort {
+
+    private IndexMaxHeapSort() {}
+
+    public static void sort(Comparable[] arr) {
+        IndexMaxHeap<Comparable> indexMaxHeap = new IndexMaxHeap<Comparable>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            indexMaxHeap.insert(i, arr[i]);
+        }
+
+        assert indexMaxHeap.testIndexes();
+
+        for (int i = arr.length; i >= 0; i--) {
+            arr[i] = indexMaxHeap.extractMax();
+        }
+    }
+}
