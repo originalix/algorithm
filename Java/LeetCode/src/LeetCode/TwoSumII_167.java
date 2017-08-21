88f24e004caff2eb3a351b7004398ba9958a3af6
Lix <xiao.liunit@gmail.com>
2017-08-21 17:18:43 +0800

    #167 暴力解法

--- Java/LeetCode/src/LeetCode/TwoSumII_167.java
+++ Java/LeetCode/src/LeetCode/TwoSumII_167.java
@@ -4,4 +4,19 @@ package LeetCode;
  * Created by Lix on 2017/8/21.
  */
 public class TwoSumII_167 {
+    
+    // 167号题目的暴力解法
+    public int[] twoSum1(int[] numbers, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == target - numbers[j]) {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
