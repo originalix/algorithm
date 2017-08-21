beb923a0f56de4937759a8a0f6bdf54c9b0a9ac8
Lix <xiao.liunit@gmail.com>
2017-08-21 17:19:05 +0800

    对撞指针方式处理 # 167

--- Java/LeetCode/src/LeetCode/TwoSumII_167.java
+++ Java/LeetCode/src/LeetCode/TwoSumII_167.java
@@ -4,6 +4,27 @@ package LeetCode;
  * Created by Lix on 2017/8/21.
  */
 public class TwoSumII_167 {
+
+    // 用指针对撞方式解决 #167问题
+    public int[] twoSum(int[] numbers, int target) {
+        int[] arr = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int v = numbers[left] + numbers[right];
+            if (v == target) {
+                arr[0] = left + 1;
+                arr[1] = right + 1;
+                break;
+            } else if (v < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return arr;
+    }
     
     // 167号题目的暴力解法
     public int[] twoSum1(int[] numbers, int target) {
