55e3bfe411db5ef3079d419eaf5afe7eead7821b
Lix <xiao.liunit@gmail.com>
2017-08-21 15:58:21 +0800

    #75: 三路快排完成题目

--- Java/LeetCode/src/LeetCode/SortColors_75.java
+++ Java/LeetCode/src/LeetCode/SortColors_75.java
@@ -31,14 +31,33 @@ public class SortColors_75 {
         }
     }
 
-
+    // 用三路快排完成题目
+    // 时间复杂度: O(n)
+    // 空间复杂度: O(1)
+    // 对整个数组只遍历了一遍
     public static void sortColors1(int[] nums) {
         // nums[0...zero] ---> 0
         // nums[zero+1 ... two - 1] ---> 1
         // nums[two...n-1] ---> 2
 
         int zero = -1;
-        int i = 0;
         int two = nums.length;
+
+        for (int i = 0; i < two; ) {
+            if (nums[i] == 1) {
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, --two);
+            } else {
+                assert (nums[i] == 0);
+                swap(nums, i++, ++zero);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
     }
 }
