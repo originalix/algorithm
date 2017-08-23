db4fad70c3c446b60458a6b4598c29660a8a12ae
Lix <xiao.liunit@gmail.com>
2017-08-23 17:34:06 +0800

    private property for BST in java

--- Java/LeetCode/src/LeetCode/BST.java
+++ Java/LeetCode/src/LeetCode/BST.java
@@ -3,6 +3,22 @@ package LeetCode;
 /**
  * Created by Lix on 2017/8/23.
  */
-public class BST {
+public class BST<Key extends Comparable<Key>, Value> {
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+
+        Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            this.left = this.right = null;
+        }
+    }
+
+    private Node root;
+    private int count;
+
     
 }
