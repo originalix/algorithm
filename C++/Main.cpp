2ee883858cc631cf2302400bb90e296cdadbd420
Lix <xiao.liunit@gmail.com>
2017-08-25 08:16:39 +0800

    add unionfind class

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <ctime>
-#include <string>
-#include "FileOps.h"
+#include <cassert>
 
 using namespace std;
 
+class UnionFind {
+
+private:
+    int *id;
+    int count;
+
+public:
+    UnionFind( int n ) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    ~UnionFind() {
+        delete[] id;
+    }
+
+    int find( int p ) {
+        assert( p >= 0 && p < count);
+        return id[p];
+    }
+};
+
 int main() {
+    cout << "Hello wsx" << endl;
     return 0;
 }
