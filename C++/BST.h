9d026d8c05af10098a2da330e162297d4269c541
Lix <xiao.liunit@gmail.com>
2017-08-25 08:12:06 +0800

    add binarysearch in bst header

--- C++/BST.h
+++ C++/BST.h
@@ -286,3 +286,19 @@ private:
         }
     }
 };
+
+template<typename T>
+int binarySearch(T arr[], int n, int target) {
+    int l = 0, r = n - 1;
+    while(l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            r = mid - 1;
+        if (arr[mid] > target)
+            l = mid + 1;
+    }
+
+    return -1;
+}
