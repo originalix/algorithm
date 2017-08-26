b1f231f7416e54902a14f7a5d3bd639f9cc2099f
Lix <xiao.liunit@gmail.com>
2017-08-26 10:37:44 +0800

    union find construction

--- Java/LeetCode/src/PlayWithAlgorithms/UnionFInd.java
+++ Java/LeetCode/src/PlayWithAlgorithms/UnionFInd.java
@@ -4,4 +4,14 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/26.
  */
 public class UnionFInd {
+
+    private int[] id;
+    private int count;
+
+    public UnionFInd(int n) {
+        count = n;
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
 }
