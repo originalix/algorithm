80ee87ff3fa51d8eb6091017f34df59444e39100
Lix <xiao.liunit@gmail.com>
2017-08-27 20:45:20 +0800

    testUF1 function

--- C++/UnionFindTestHelper.h
+++ C++/UnionFindTestHelper.h
@@ -6,5 +6,27 @@ using namespace std;
 
 // 测试并查集的辅助函数
 namespace UnionFindTestHelper {
+    void testUF1( int n ) {
+        srand( time(NULL) );
 
+        UF1::UnionFind uf = UF1::UnionFind(n);
+
+        time_t startTime = clock();
+
+        for (int i = 0; i < n; i++) {
+            int a = rand() % n;
+            int b = rand() % n;
+            uf.unionElements(a, b);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int a = rand() % n;
+            int b = rand() % n;
+            uf.isConnected(a, b);
+        }
+
+        time_t endTime = clock();
+
+        cout << "UF1, " << 2*n << " ops, " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+    }
 }
