57edefda3d25a9c8f5d6a051110427956df37ac9
Lix <xiao.liunit@gmail.com>
2017-08-29 07:29:56 +0800

    construction unionfind1

--- C++/UnionFind2.h
+++ C++/UnionFind2.h
@@ -4,5 +4,19 @@
 using namespace std;
 
 namespace UF2 {
+    class UnionFind2 {
+    private:
+        int* parent;
+        int count;
 
+    public:
+        UnionFind2( int count ) {
+            parent = new int[count];
+            this->count = count;
+            for (int i = 0; i < count; i++) {
+                parent[i] = i;
+            }
+        }
+
+};
 }
