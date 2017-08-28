6b6f5e82a8b685f350c6ffe3a2f623393ac01fea
Lix <xiao.liunit@gmail.com>
2017-08-29 07:57:25 +0800

    construction unionfind3

--- C++/UnionFind3.h
+++ C++/UnionFind3.h
@@ -2,5 +2,22 @@
 #include <cassert>
 
 namespace UF3 {
+    class UnionFind {
+        private:
+            int* parent;
+            int* sz;
+            int count;
+        
+        public:
+            UnionFind( int count ) {
+                parent = new int[count];
+                sz = new int[count];
+                this->count = count;
 
+                for (int i = 0; i < count; i++) {
+                    parent[i] = i;
+                    sz[i] = 1;
+                }
+            }
+    };
 }
