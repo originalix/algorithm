8707df19d105f3b6beaf4a89e2c56286ca9c0747
Lix <xiao.liunit@gmail.com>
2017-08-29 10:53:28 +0800

    construction for unionfind4

--- C++/UnionFind4.h
+++ C++/UnionFind4.h
@@ -4,5 +4,22 @@
 using namespace std;
 
 namespace UF4 {
-    
+    class UnionFind {
+        private:
+            int* rank;
+            int* parent;
+            int count;
+
+        public:
+            UnionFind( int count ) {
+                parent = new int[count];
+                rank = new int[count];
+                this->count = count;
+
+                for (int i = 0; i < count; i++) {
+                    parent[i] = i;
+                    rank[i] = 1;
+                }
+            }
+    };
 }
