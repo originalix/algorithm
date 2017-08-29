e49e145fadd330817c36f82004aa4ebe269a5266
Lix <xiao.liunit@gmail.com>
2017-08-29 16:53:36 +0800

    unionfind2 finish

--- Java/LeetCode/src/PlayWithAlgorithms/UnionFind2.java
+++ Java/LeetCode/src/PlayWithAlgorithms/UnionFind2.java
@@ -16,5 +16,29 @@ public class UnionFind2 {
             parent[i] = i;
         }
     }
-    
+
+    public int find(int p) {
+        assert (p >= 0 && p < count);
+
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+
+        return p;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        parent[pRoot] = qRoot;
+    }
 }
