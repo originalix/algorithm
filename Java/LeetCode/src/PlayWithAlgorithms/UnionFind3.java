d0b3f558666114c6588fec147cb93d511b76cf3c
Lix <xiao.liunit@gmail.com>
2017-08-29 16:58:42 +0800

    finish unionfind3

--- Java/LeetCode/src/PlayWithAlgorithms/UnionFind3.java
+++ Java/LeetCode/src/PlayWithAlgorithms/UnionFind3.java
@@ -4,4 +4,48 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/29.
  */
 public class UnionFind3 {
+    private int[] parent;
+    private int[] sz;
+    private int count;
+
+    public UnionFind3(int count) {
+        parent = new int[count];
+        this.count = count;
+
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int find(int p) {
+        assert (p >= 0 && p < count);
+
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+
+        return p;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        if (sz[pRoot] < sz[qRoot]) {
+            parent[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        } else {
+            parent[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+    }
 }
