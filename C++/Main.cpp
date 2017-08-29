f1e20387e47151372130bc15d1ccbb3122942798
Lix <xiao.liunit@gmail.com>
2017-08-29 11:02:12 +0800

    test uf4

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Hello wsx" << endl;
 
     // 使用10000的数据规模
-    int n = 1000000;
+    int n = 10000000;
     // 虽然isConnected只需要O(1)的时间, 但由于union操作需要O(n)的时间
     // 总体测试过程的算法复杂度是O(n^2)的
     // UnionFindTestHelper::testUF1(n);
@@ -15,6 +15,8 @@ int main() {
     // UnionFindTestHelper::testUF2(n);
 
     UnionFindTestHelper::testUF3(n);
+
+    UnionFindTestHelper::testUF4(n);
 
     return 0;
 }
