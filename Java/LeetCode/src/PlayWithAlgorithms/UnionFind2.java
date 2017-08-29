eb78d6d5d715d7c9dac5705154d63dc069e2e93c
Lix <xiao.liunit@gmail.com>
2017-08-29 16:48:37 +0800

    add construction in unionfind2

--- Java/LeetCode/src/PlayWithAlgorithms/UnionFind2.java
+++ Java/LeetCode/src/PlayWithAlgorithms/UnionFind2.java
@@ -4,5 +4,17 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/29.
  */
 public class UnionFind2 {
+
+    private int[] parent;
+    private int count;
+
+    public UnionFind2(int count) {
+        parent = new int[count];
+        this.count = count;
+
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+        }
+    }
     
 }
