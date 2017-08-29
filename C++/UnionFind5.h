b1aef2c421e5a0e68fbe1d0d44a9b1e41fc73914
Lix <xiao.liunit@gmail.com>
2017-08-29 11:34:06 +0800

    construction unionfind 4

--- C++/UnionFind5.h
+++ C++/UnionFind5.h
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+// 第五版Union-Find 路径压缩优化
 namespace UF5 {
-    
+    class UnionFind {
+        private:
+            int* rank;
+            int* parent;
+            int count;
+
+        public:
+            UnionFind( int count ) {
+                parent = new int[count];
+                rank = new int[count];
+                this->count = count;
+
+                for (int i = 0; i < count; i++) {
+                    parent[i] = i;
+                    rank[i] = 1;
+                }
+            }
+    };
 }
