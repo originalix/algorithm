3fc499f9b5f0aee979a227faf23ce7c6697426c6
Lix <xiao.liunit@gmail.com>
2017-08-29 17:14:26 +0800

    add comment

--- Java/LeetCode/src/PlayWithAlgorithms/UnionFind5.java
+++ Java/LeetCode/src/PlayWithAlgorithms/UnionFind5.java
@@ -4,6 +4,10 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/29.
  */
 public class UnionFind5 {
+
+    // rank[i]表示以i为根的集合所表示的树的层数
+    // 在后续的代码中, 我们并不会维护rank的语意, 也就是rank的值在路径压缩的过程中, 有可能不在是树的层数值
+    // 这也是我们的rank不叫height或者depth的原因, 他只是作为比较的一个标准
     private int[] parent;
     private int[] rank;
     private int count;
@@ -22,12 +26,21 @@ public class UnionFind5 {
     public int find(int p) {
         assert (p >= 0 && p < count);
 
+        // path compression 1
         while (p != parent[p]) {
             parent[p] = parent[ parent[p] ];
             p = parent[p];
         }
 
         return p;
+
+
+//        if ( p != parent[p]) {
+//            parent[p] = find(parent[p]);
+//        }
+//
+//        return parent[p];
+
     }
 
     public boolean isConnected(int p, int q) {
@@ -42,6 +55,8 @@ public class UnionFind5 {
             return;
         }
 
+        // 根据两个元素所在树的元素个数不同判断合并方向
+        // 将元素个数少的集合合并到元素个数多的集合上
         if (rank[pRoot] < rank[qRoot]) {
             parent[pRoot] = qRoot;
         } else if (rank[qRoot] < rank[pRoot]){
