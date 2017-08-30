672fb86be78e1b9b45fa8118e248aac6fc91200c
Lix <xiao.liunit@gmail.com>
2017-08-30 14:38:24 +0800

    add comment

--- C++/DenseGraph.h
+++ C++/DenseGraph.h
@@ -4,19 +4,22 @@
 
 using namespace std;
 
+// 稠密图 — 邻接矩阵
 class DenseGraph {
 private:
-    int n, m;
-    bool directed;
-    vector< vector<bool> > g;
+    int n, m;  // 节点数和边数
+    bool directed; // 是否为有向图
+    vector< vector<bool> > g;  // 图的具体数据
 
 public:
+    // 构造函数
     DenseGraph( int n, bool directed ) {
         this->n = n;
-        this->m = 0;
+        this->m = 0; // 初始化没有任何边
         this->directed = directed;
 
         for (int i = 0; i < n; i++) {
+            // g初始化为n*n的布尔矩阵，每一个g[i][j]均为false,表示没有任何边
             g.push_back( vector<bool>(n, false) );
         }
     }
@@ -25,9 +28,10 @@ public:
 
     }
 
-    int V() { return n; }
-    int E() { return m; }
+    int V() { return n; } // 返回节点个数
+    int E() { return m; } // 返回边的个数
 
+    // 向图中添加一个边
     void addEdge( int v, int w ) {
         assert( v >= 0 && v < n);
         assert( w >= 0 && w < n);
@@ -41,7 +45,7 @@ public:
         m++;
     }
 
-
+    // 验证图中是否有从v到w的边
     bool hasEdge( int v, int w ) {
         assert( v >= 0 && v < n );
         assert( w >= 0 && w < n );
