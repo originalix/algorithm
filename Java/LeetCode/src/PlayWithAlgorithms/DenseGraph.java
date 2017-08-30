56fea810e2e217b9259c19810438dfbdb27b06dc
Lix <xiao.liunit@gmail.com>
2017-08-30 14:55:49 +0800

    construction function for dense graph

--- Java/LeetCode/src/PlayWithAlgorithms/DenseGraph.java
+++ Java/LeetCode/src/PlayWithAlgorithms/DenseGraph.java
@@ -4,4 +4,19 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/8/30.
  */
 public class DenseGraph {
+
+    private int n;
+    private int m;
+
+    private boolean directed;
+    private boolean[][] g;
+
+    public DenseGraph(int n, boolean directed) {
+        assert n >= 0;
+        this.n = n;
+        this.m = 0;
+        this.directed = directed;
+        g = new boolean[n][n];
+    }
+    
 }
