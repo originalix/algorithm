120015b964d479e41748e117bfcb64865df01f93
Lix <xiao.liunit@gmail.com>
2017-08-30 15:19:33 +0800

    function for spraseGraph

--- Java/LeetCode/src/PlayWithAlgorithms/SparseGraph.java
+++ Java/LeetCode/src/PlayWithAlgorithms/SparseGraph.java
@@ -1,7 +1,38 @@
 package PlayWithAlgorithms;
 
+import java.util.Vector;
+
 /**
  * Created by Lix on 2017/8/30.
  */
 public class SparseGraph {
+
+    private int n;
+    private int m;
+    private boolean directed;
+    private Vector<Integer>[] g;
+
+    public SparseGraph(int n, boolean directed) {
+        this.n = n;
+        this.m = 0;
+        this.directed = directed;
+        g = (Vector<Integer>[])new Vector[n];
+        for (int i = 0; i < n; i++) {
+            g[i] = new Vector<Integer>();
+        }
+    }
+
+    public int V() { return n; }
+    public int E() { return m; }
+
+    public void addEdge(int v, int w) {
+        assert v >= 0 && v < n;
+        assert w >= 0 && w < n;
+        g[v].add(w);
+        if (v != w && !directed) {
+            g[w].add(v);
+        }
+
+        m++;
+    }
 }
