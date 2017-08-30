0d6728dccdf34c81949ba1da74e6a99fce368e59
Lix <xiao.liunit@gmail.com>
2017-08-30 14:42:02 +0800

    add comment

--- C++/SparseGraph.h
+++ C++/SparseGraph.h
@@ -4,14 +4,16 @@
 
 using namespace std;
 
+// 稀疏图 - 邻接表
 class SparseGraph {
 
 private:
-    int n, m;
-    bool directed;
-    vector< vector<int> > g;
+    int n, m; // 节点数和边数
+    bool directed;  // 是否为有向图
+    vector< vector<int> > g; // 图的具体数据
 
 public:
+    // 构造函数
     SparseGraph( int n, bool directed ) {
         this->n = n;
         this->m = 0;
@@ -26,9 +28,10 @@ public:
 
     }
 
-    int V() { return n; }
-    int E() { return m; }
+    int V() { return n; } // 返回节点的个数
+    int E() { return m; } // 返回边的个数
 
+    // 向图中添加一个边
     void addEdge( int v, int w ) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
@@ -40,6 +43,7 @@ public:
         m++;
     }
 
+    // 验证图中是否有从v到w的边
     bool hasEdge( int v, int w ) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
