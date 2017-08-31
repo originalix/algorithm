5fb0a619d0523fb18d9bbd89efd2c60299a633a2
Lix <xiao.liunit@gmail.com>
2017-09-01 07:51:08 +0800

    test path

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -2,52 +2,22 @@
 #include "SparseGraph.h"
 #include "DenseGraph.h"
 #include "ReadGraph.h"
-#include "Component.h"
+#include "Path.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello wsx" << endl;
 
-    string filename = "testG1.txt";
-    SparseGraph g1(13, false);
-    ReadGraph<SparseGraph> readGraph1( g1, filename );
-    Component<SparseGraph> component1(g1);
-    cout << "test G1 in Sparse Graph:" << endl;
-    cout<<"TestG1.txt, Component Count: "<<component1.count()<<endl;
-
-    g1.show();
-
+    string filename = "testG2.txt";
+    SparseGraph g = SparseGraph(7, false);
+    ReadGraph<SparseGraph> readGraph(g, filename);
+    g.show();
     cout << endl;
 
-    DenseGraph g2(13, false);
-    ReadGraph<DenseGraph> readGraph2( g2, filename );
-    Component<DenseGraph> component2(g2);
-    cout << "test G2 in Dense Graph:" << endl;
-    cout<<"TestG1.txt, Component Count: "<<component2.count()<<endl;
-    g2.show();
-
-    cout << endl;
-
-    filename = "testG2.txt";
-    SparseGraph g3(7, false);
-    ReadGraph<SparseGraph> readGraph3( g3, filename );
-    Component<SparseGraph> component3(g3);
-    cout << "test G2 in Sparse Graph:" << endl;
-    cout<<"TestG2.txt, Component Count: "<<component3.count()<<endl;
-
-    g3.show();
-
-    cout << endl;
-
-    DenseGraph g4(7, false);
-    ReadGraph<DenseGraph> readGraph4( g4, filename );
-    Component<DenseGraph> component4(g4);
-    cout << "test G2 in Dense Graph:" << endl;
-    cout<<"TestG1.txt, Component Count: "<<component4.count()<<endl;
-    g2.show();
-
-    cout << endl;
+    Path<SparseGraph> path(g, 0);
+    cout << "Path from 0 to 6: " << endl;
+    path.showPath(6);
 
     return 0;
 }
