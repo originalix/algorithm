0affca0bf8ce288861c575af3c3f8f991d21e281
Lix <xiao.liunit@gmail.com>
2017-09-01 07:40:00 +0800

    construction and desconstruction function

--- C++/Path.h
+++ C++/Path.h
@@ -8,4 +8,31 @@ using namespace std;
 template<typename Graph>
 class Path {
 
+private:
+    Graph &G;
+    int s;
+    bool* visited;
+    int* from;
+
+public:
+    Path(Graph &graph, int s): G(graph) {
+        assert( s >= 0 && s < G.V() );
+
+        visited = new bool[G.V()];
+        from = new int[G.V()];
+
+        for (int i = 0; i < G.V(); i++) {
+            visited[i] = false;
+            from[i] = -1;
+        }
+
+        this->s = s;
+        // 寻路算法
+        dfs(s);
+    }
+
+    ~Path() {
+        delete[] visited;
+        delete[] from;
+    }
 };
