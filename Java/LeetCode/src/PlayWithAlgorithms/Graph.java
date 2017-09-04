ec512699330a505fa665d03d9eacbf274b86cf02
Lix <xiao.liunit@gmail.com>
2017-09-04 16:37:04 +0800

    add interface graph

---
+++ Java/LeetCode/src/PlayWithAlgorithms/Graph.java
@@ -0,0 +1,13 @@
+package PlayWithAlgorithms;
+
+/**
+ * Created by Lix on 2017/9/4.
+ */
+public interface Graph {
+    public int V();
+    public int E();
+    public void addEdge( int v, int w );
+    boolean hasEdge( int v, int w );
+    void show();
+    public Iterable<Integer> adj(int v);
+}
