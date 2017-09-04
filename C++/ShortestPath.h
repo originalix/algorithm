ecc626455bf3ae0aba563707cfbe0d6ed78eeb66
Lix <xiao.liunit@gmail.com>
2017-09-04 08:05:31 +0800

    construction for ShortestPath

--- C++/ShortestPath.h
+++ C++/ShortestPath.h
@@ -18,6 +18,36 @@ private:
 
 public:
     ShortestPath( Graph &graph, int s ): G(graph) {
-        
+        assert ( s >= 0 && s < graph.V() );
+        visited = new bool[graph.V()];
+        from = new int[graph.V()];
+        ord = new int[graph.V()];
+
+        for (int i = 0; i < graph.V(); i++) {
+            visited[i] = false;
+            from[i] = -1;
+            ord[i] = -1;
+        }
+
+        this->s = s;
+
+        queue<int> q;
+        q.push(s);
+        visited[s] = true;
+        ord[s] = 0;
+        while ( !q.empty() ) {
+            int v = q.front();
+            q.pop();
+
+            typename Graph::adjIterator adj(G, v);
+            for (int i = adj.begin(); !adj.end(); i = adj.next()) {
+                if (!visited[i]) {
+                    q.push(i);
+                    visited[i] = true;
+                    from[i] = v;
+                    ord[i] = ord[v] + 1;
+                }
+            }
+        }
     }
 };
