64ff52afcfbd7a71c62a9dd2f9f50c7c86cf3452
Lix <xiao.liunit@gmail.com>
2017-09-04 08:00:32 +0800

    property for Shortestpath

--- C++/ShortestPath.h
+++ C++/ShortestPath.h
@@ -6,6 +6,18 @@
 
 using namespace std;
 
+template <typename Graph>
 class ShortestPath {
 
+private:
+    Graph &G;
+    int s;
+    bool *visited;
+    int *from;
+    int *ord;
+
+public:
+    ShortestPath( Graph &graph, int s ): G(graph) {
+        
+    }
 };
