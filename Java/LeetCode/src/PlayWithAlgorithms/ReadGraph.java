a1bb27dbb548ba46bfea949006e1e96c0d472266
Lix <xiao.liunit@gmail.com>
2017-09-05 05:54:33 +0800

    delete test code

--- Java/LeetCode/src/PlayWithAlgorithms/ReadGraph.java
+++ Java/LeetCode/src/PlayWithAlgorithms/ReadGraph.java
@@ -1,7 +1,14 @@
 package PlayWithAlgorithms;
 
+import java.util.Scanner;
+
 /**
  * Created by Lix on 2017/9/4.
  */
 public class ReadGraph {
+    private Scanner scanner;
+
+    private void readFile(String filename) {
+
+    }
 }
