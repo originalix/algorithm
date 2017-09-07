6dd24155fda076f05e1018504ef6ee0315dc17c6
Lix <xiao.liunit@gmail.com>
2017-09-07 16:40:44 +0800

    read graph

--- Java/LeetCode/src/PlayWithAlgorithms/ReadGraph.java
+++ Java/LeetCode/src/PlayWithAlgorithms/ReadGraph.java
@@ -4,7 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Locale;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -12,6 +14,36 @@ import java.util.Scanner;
  */
 public class ReadGraph {
     private Scanner scanner;
+
+    public ReadGraph(Graph graph, String filename) {
+        readFile(filename);
+
+        try {
+            int V = scanner.nextInt();
+            if (V < 0) {
+                throw new IllegalArgumentException("Number of vertices in a Graph must be nonegative");
+            }
+            assert V == graph.V();
+
+            int E = scanner.nextInt();
+            if (E < 0) {
+                throw new IllegalArgumentException("number of edges in a Graph must be nonegative");
+            }
+            for (int i = 0; i < E; i++) {
+                int v = scanner.nextInt();
+                int w = scanner.nextInt();
+                assert v >= 0 && v < V;
+                assert w >= 0 && w < V;
+                graph.addEdge(v, w);
+            }
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'int' value from input stream, but the next token is \" " + token + "\" " );
+        }
+        catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'int' value from input stram, but there are no more tokens available");
+        }
+    }
 
     private void readFile(String filename) {
         assert filename != null;
