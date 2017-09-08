ac95b51fc6a28cf1a2b9a2c6af8765f4811e308e
Lix <xiao.liunit@gmail.com>
2017-09-08 13:40:46 +0800

    property for Components

--- Java/LeetCode/src/PlayWithAlgorithms/Components.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Components.java
@@ -4,4 +4,9 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/9/8.
  */
 public class Components {
+
+    private Graph G;
+    private boolean[] visited;
+    private int ccount;
+    private int[] id;
 }
