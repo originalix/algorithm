f443dc80565da078eda6a4bd7be2ed4f980d6b95
Lix <xiao.liunit@gmail.com>
2017-09-08 13:44:06 +0800

    construction for Components

--- Java/LeetCode/src/PlayWithAlgorithms/Components.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Components.java
@@ -9,4 +9,22 @@ public class Components {
     private boolean[] visited;
     private int ccount;
     private int[] id;
+
+    public Components(Graph graph) {
+        this.G = graph;
+        visited = new boolean[graph.V()];
+        id = new int[graph.V()];
+        ccount = 0;
+        for (int i = 0; i < graph.V(); i++) {
+            visited[i] = false;
+            id[i] = -1;
+        }
+
+        for (int i = 0; i < graph.V(); i++) {
+            if (!visited[i]) {
+                dfs(i);
+                ccount += 1;
+            }
+        }
+    }
 }
