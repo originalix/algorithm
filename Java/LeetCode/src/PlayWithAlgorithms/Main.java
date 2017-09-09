36b523bbdb183d8503ed678a2b37d7ada76935dd
Lix <xiao.liunit@gmail.com>
2017-09-09 21:19:10 +0800

    test path

--- Java/LeetCode/src/PlayWithAlgorithms/Main.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Main.java
@@ -8,22 +8,17 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        String filename = "testG1.txt";
-        SparseGraph g1 = new SparseGraph(13, false);
+        String filename = "testG2.txt";
+        SparseGraph g1 = new SparseGraph(7, false);
         ReadGraph readGraph1 = new ReadGraph(g1, filename);
         Components components1 = new Components(g1);
-        System.out.println("TestG1.txt in Sparse Graph, Components count is " + components1.count());
+        System.out.println("TestG2.txt in Sparse Graph, Components count is " + components1.count());
         g1.show();
 
         System.out.println();
 
-        filename = "testG2.txt";
-        SparseGraph g2 = new SparseGraph(7, false);
-        ReadGraph readGraph2 = new ReadGraph(g2, filename);
-        Components components2 = new Components(g2);
-        System.out.println("TestG2.txt in Sparse Graph, Components count is " + components2.count());
-        g2.show();
-
-        System.out.println();
+        Path path = new Path(g1, 0);
+        System.out.println("Path from 0 to 6 : ");
+        path.showPath(6);
     }
 }
