f1d7f960ebd77ea91a5ce57d79d8c7c0dd12cef4
Lix <xiao.liunit@gmail.com>
2017-09-09 20:44:53 +0800

    test graph components

--- Java/LeetCode/src/PlayWithAlgorithms/Main.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Main.java
@@ -11,10 +11,21 @@ public class Main {
         String filename = "testG1.txt";
         SparseGraph g1 = new SparseGraph(13, false);
         ReadGraph readGraph1 = new ReadGraph(g1, filename);
-        System.out.println("test G1 in Sparse Graph");
+        Components components1 = new Components(g1);
+        System.out.println("TestG1.txt in Sparse Graph, Components count is " + components1.count());
         g1.show();
 
         System.out.println();
+
+        filename = "testG2.txt";
+        SparseGraph g2 = new SparseGraph(7, false);
+        ReadGraph readGraph2 = new ReadGraph(g2, filename);
+        Components components2 = new Components(g2);
+        System.out.println("TestG2.txt in Sparse Graph, Components count is " + components2.count());
+        g2.show();
+
+        System.out.println();
+
 
     }
 }
