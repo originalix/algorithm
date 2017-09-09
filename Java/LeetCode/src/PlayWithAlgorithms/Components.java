ff6d2b76e74fb0043a96d5561f9333298aa83f88
Lix <xiao.liunit@gmail.com>
2017-09-09 20:41:03 +0800

    count and isConnected function

--- Java/LeetCode/src/PlayWithAlgorithms/Components.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Components.java
@@ -12,6 +12,7 @@ public class Components {
     private int ccount;  // 记录联通分量个数
     private int[] id;  // 每个节点所对应的联通分量的标记
 
+    // 构造函数，求出无权图的联通分量
     public Components(Graph graph) {
         this.G = graph;
         visited = new boolean[graph.V()];
@@ -30,6 +31,7 @@ public class Components {
         }
     }
 
+    // 图的深度优先遍历
     private void dfs(int v) {
         visited[v] = true;
         id[v] = ccount;
@@ -38,5 +40,17 @@ public class Components {
                 dfs(i);
             }
         }
+    }
+
+    // 返回图的联通分量
+    public int count() {
+        return ccount;
+    }
+
+    // 查询点v和点w是否联通
+    public boolean isConnected(int v, int w) {
+        assert v >= 0 && v < G.V();
+        assert w >= 0 && w < G.V();
+        return id[v] == id[w];
     }
 }
