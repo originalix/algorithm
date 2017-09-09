325d6fa2fd171e7a5d6ef4a83d377fadb75f3994
Lix <xiao.liunit@gmail.com>
2017-09-09 21:00:09 +0800

    property for Path

--- Java/LeetCode/src/PlayWithAlgorithms/Path.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Path.java
@@ -4,4 +4,10 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/9/9.
  */
 public class Path {
+
+    private Graph G;  // 图的引用
+    private int s;  // 起始点
+    private boolean[] visited;  // 记录dfs的过程中节点是否被访问
+    private int[] from;  // 记录路径，from[i]表示查找的路径上i的上一个节点
+
 }
