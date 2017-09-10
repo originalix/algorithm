78ec1c703100046e30eeeeb7e8f491bb1ebc446b
Lix <xiao.liunit@gmail.com>
2017-09-10 20:44:39 +0800

    property for ShortestPath

--- Java/LeetCode/src/PlayWithAlgorithms/ShortestPath.java
+++ Java/LeetCode/src/PlayWithAlgorithms/ShortestPath.java
@@ -4,4 +4,10 @@ package PlayWithAlgorithms;
  * Created by Lix on 2017/9/10.
  */
 public class ShortestPath {
+
+    private Graph G;
+    private int s;
+    private boolean[] visited;
+    private int[] from;
+    private int[] ord;
 }
