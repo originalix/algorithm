acf1597eab68de91101833224ea2605451013689
Lix <xiao.liunit@gmail.com>
2017-09-12 07:38:40 +0800

    construction for minheap

--- C++/MinHeap.h
+++ C++/MinHeap.h
@@ -6,8 +6,21 @@
 
 using namespace std;
 
+// 最小堆
+template <typename Item>
 class MinHeap {
+private:
+    Item *data;
+    int count;
+    int capacity;
 
+public:
+    // 构造函数，构造一个空堆，可容纳capacity个元素f
+    MinHeap(int capacity) {
+        data = new Item[capacity + 1];
+        count = 0;
+        this->capacity = capacity;
+    }
 };
 
 #endif
