d878df96a42b9dd94f3aec215060d80620a51610
Lix <xiao.liunit@gmail.com>
2017-09-11 21:04:57 +0800

    construction for edge

--- C++/Edge.h
+++ C++/Edge.h
@@ -5,5 +5,14 @@ using namespace std;
 
 template <typename Weight>
 class Edge {
+private:
+    int a, b;
+    Weight weight;
 
+public:
+    Edge(int a, int b, Weight weight) {
+        this->a = a;
+        this->b = b;
+        this->weight = weight;
+    }
 };
