0246d2bcd5ad92f1dfdba1d56708ae241a8653f3
Lix <xiao.liunit@gmail.com>
2017-09-11 21:45:39 +0800

    change SparseGraph

--- C++/SparseGraph.h
+++ C++/SparseGraph.h
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include "Edge.h"
 
 using namespace std;
 
 // 稀疏图 - 邻接表
+template <typename Weight>
 class SparseGraph {
 
 private:
     int n, m; // 节点数和边数
     bool directed;  // 是否为有向图
-    vector< vector<int> > g; // 图的具体数据
+    vector< vector< Edge<Weight> *> > g; // 图的具体数据
 
 public:
     // 构造函数
@@ -19,26 +21,28 @@ public:
         this->m = 0;
         this->directed = directed;
 
-        for (int i = 0; i < n; i++) {
-            g.push_back( vector<int>() );
-        }
+        g = vector< vector<Edge<Weight> *> >(n, vector<Edge<Weight> *>());
     }
 
     ~SparseGraph() {
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                delete g[i][j];
+            }
+        }
     }
 
     int V() { return n; } // 返回节点的个数
     int E() { return m; } // 返回边的个数
 
     // 向图中添加一个边
-    void addEdge( int v, int w ) {
+    void addEdge( int v, int w, Weight weight ) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
 
-        g[v].push_back(w);
+        g[v].push_back(new Edge<Weight>(v, w, weight));
         if ( v != w && !directed)
-            g[w].push_back(v);
+            g[w].push_back(new Edge<Weight>(w, v, weight));
         
         m++;
     }
@@ -49,7 +53,7 @@ public:
         assert(w >= 0 && w < n);
 
         for (int i = 0; i < g[v].size(); i++) {
-            if (g[v][i] == w)
+            if (g[v][i]->other(v) == w)
                 return true;
         }
 
@@ -60,7 +64,7 @@ public:
         for (int i = 0; i < n; i++) {
             cout << "vertex " << i << ":\t";
             for (int j = 0; j < g[i].size(); j++)
-                cout << g[i][j] << "\t";
+                cout << "( to: " << g[i][j]->w() << ", wt: " <<g[i][j]->wt() << ")\t";
             cout << endl;
         }
     }
