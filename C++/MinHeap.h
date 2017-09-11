f58de06863948aaa7755deea05dd6cf296cbab09
Lix <xiao.liunit@gmail.com>
2017-09-12 07:42:16 +0800

    core function for MinHeap

--- C++/MinHeap.h
+++ C++/MinHeap.h
@@ -14,12 +14,35 @@ private:
     int count;
     int capacity;
 
+    void shiftUp(int k) {
+        while ( k > 1 && data[k/2] > data[k] ) {
+            swap( data[k / 2], data[k] );
+            k /= 2;
+        }
+    }
+
+    void shiftDown(int k) {
+        while ( 2*k <= count ) {
+            int j = 2*k;
+            if ( j+1 <= count && data[j+1] < data[j] )
+                j++;
+            if ( data[k] <= data[j] )
+                break;
+            swap( data[k], data[j] );
+            k = j;
+        }
+    }
+
 public:
     // 构造函数，构造一个空堆，可容纳capacity个元素f
     MinHeap(int capacity) {
         data = new Item[capacity + 1];
         count = 0;
         this->capacity = capacity;
+    }
+
+    ~MinHeap() {
+        delete[] data;
     }
 };
 
