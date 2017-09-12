fa0358391757fadb8e924b8857f05b518477142b
Lix <xiao.liunit@gmail.com>
2017-09-13 07:26:36 +0800

    test LazyPrimMST

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -3,6 +3,7 @@
 #include "DenseGraph.h"
 #include "SparseGraph.h"
 #include "ReadGraph.h"
+#include "LazyPrimMST.h"
 
 using namespace std;
 
@@ -16,15 +17,19 @@ int main() {
 
     cout << fixed << setprecision(2);
 
-    DenseGraph<double> g1 = DenseGraph<double>(V, false);
-    ReadGraph<DenseGraph<double>, double> readGraph1(g1, filename);
-    g1.show();
+    SparseGraph<double> g = SparseGraph<double>(V, false);
+    ReadGraph<SparseGraph<double>, double> readGraph2(g, filename);
+    g.show();
     cout << endl;
 
-    SparseGraph<double> g2 = SparseGraph<double>(V, false);
-    ReadGraph<SparseGraph<double>, double> readGraph2(g2, filename);
-    g2.show();
-    cout << endl;
+    // cout << "Test Lazy Prim MST:" << endl;
+    // LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+    // vector< Edge<double> > mst = lazyPrimMST.mstEdges();
+    // for (int i = 0; i < mst.size(); i++) {
+    //     cout << mst[i] << endl;
+    // }
+    // cout << "The MST weight is : " << lazyPrimMST.result() << endl;
+    // cout << endl;
 
     return 0;
 }
