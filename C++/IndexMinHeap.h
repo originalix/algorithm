61fb47db16dad58f5f32b31c54408acaf9610f24
Lix <xiao.liunit@gmail.com>
2017-09-13 20:55:23 +0800

    indexMinHeap core code

--- C++/IndexMinHeap.h
+++ C++/IndexMinHeap.h
@@ -13,4 +13,31 @@ private:
 
     int count;     // 最小索引堆当前元素个数
     int capacity;  // 最小索引堆的空间大小
+
+    void shiftUp( int k ) {
+        while ( k > 1 && data[indexes[k/2]] > data[indexes[k]]) {
+            swap( indexes[k/2], indexes[k]);
+            reverse[indexes[k/2]] = k/2;
+            reverse[indexes[k]] = k;
+            k /= 2;
+        }
+    }
+
+    void shiftDown( int k ) {
+        while ( 2*k <= count ) {
+            int j = 2*k;
+            if (j+1 <= count && data[ indexes[j] ] > data[ indexes[j+1] ])
+                j += 1;
+
+            if ( data[ indexes[k] ] <= data[ indexes[j] ] )
+                break;
+            
+            swap( indexes[k], indexes[j] );
+            reverse[ indexes[k] ] = k;
+            reverse[ indexes[j] ] = j;
+            k = j;
+        }
+    }
+
+
 };
