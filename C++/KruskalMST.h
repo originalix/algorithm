dedc542f5de034ac0d55fcc6902f10d6959c43e8
Lix <xiao.liunit@gmail.com>
2017-09-15 07:34:09 +0800

    create KruskalMST class

---
+++ C++/KruskalMST.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "MinHeap.h"
+#include "UnionFind5.h"
+#include "Edge.h"
+
+using namespace std;
+
+template <typename Graph, typename Weight>
+class KruskalMST {
+
+};
