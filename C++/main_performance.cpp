3f355acf909469aaebf450af84e749b71a5450d7
Lix <xiao.liunit@gmail.com>
2017-09-14 11:19:37 +0800

    create main_performance

---
+++ C++/main_performance.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <ctime>
+#include <string>
+#include "DenseGraph.h"
+#include "SparseGraph.h"
+#include "ReadGraph.h"
+#include "LazyPrimMST.h"
+#include "PrimMST.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello wsx" << endl;
+    return 0;
+}
