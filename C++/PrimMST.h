f8c269cb55b63c504558021b313fc94b1deec107
Lix <xiao.liunit@gmail.com>
2017-09-14 10:39:23 +0800

    add visit function for PrimMST

--- C++/PrimMST.h
+++ C++/PrimMST.h
@@ -15,4 +15,25 @@ private:
     bool* marked;
     vector< Edge<Weight> > mst;
     Weight mstWeight;
+
+    // 访问节点v
+    void visit( int v ) {
+        assert( !marked[v] );
+        marked[v] = true;
+
+        typename Graph::adjIterator adj(G, v);
+        for (Edge<Weight>* e = adj.begin(); !adj.end(); e = adj.next()) {
+            int w = e->other(v);
+            if ( !marked[w] ) {
+                if ( !edgeTo[w] ) {
+                    edgeTo[w] = e;
+                    ipq.insert(w, e->wt());
+                }
+                else if ( e->wt() < edgeTo[w]->wt() ) {
+                    edgeTo[w] = e;
+                    ipq.change(w, e->wt());
+                }
+            }
+        }
+    }
 };
