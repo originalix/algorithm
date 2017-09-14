b8127b8744c66304cf39aa3eb8834755b5e6fc0e
Lix <xiao.liunit@gmail.com>
2017-09-15 07:42:27 +0800

    construction for KruskalMST

--- C++/KruskalMST.h
+++ C++/KruskalMST.h
@@ -14,6 +14,27 @@ private:
 
 public:
     KruskalMST(Graph &graph) {
-        
+        MinHeap< Edge<Weight> > pq(graph.E());
+        for (int i = 0; i < graph.V(); i++) {
+            typename Graph::adjIterator adj(graph, i);
+            for (Edge<Weight> *e = adj.begin(); !adj.end(); e = adj.next()) {
+                if (e->v() < e->w())
+                    pq.insert(*e);
+            }
+        }
+
+        UF5::UnionFind uf = UnionFind(graph.V());
+        while( !pq.isEmpty() ) {
+            Edge<Weight> e = pq.extractMin();
+            if ( uf.isConnected(e.v(), e.w()) )
+                continue;
+            mst.push_back(e);
+            uf.unionElements(e.v(), e.w());
+        }
+
+        mstWeight = mst[0].wt();
+        for (int i = 0; i < mst.size(); i++) {
+            mstWeight += mst[i].wt();
+        }
     }
 };
