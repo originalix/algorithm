a8ad47f7cf1bee682984c50a68eceb3bad6b66a5
Lix <xiao.liunit@gmail.com>
2017-09-18 07:51:12 +0800

    construction for lazyprimmst

--- Java/LeetCode/src/PlayWithAlgorithms/LazyPrimMST.java
+++ Java/LeetCode/src/PlayWithAlgorithms/LazyPrimMST.java
@@ -10,7 +10,7 @@ public class LazyPrimMST<Weight extends Number & Comparable> {
     private MinHeap<Edge<Weight>> pq;
     private boolean[] marked;
     private Vector<Edge<Weight>> mst;
-    private Weight mstWeight;
+    private Number mstWeight;
 
     public LazyPrimMST(WeightedGraph<Weight> graph) {
         G = graph;
@@ -19,6 +19,24 @@ public class LazyPrimMST<Weight extends Number & Comparable> {
         mst = new Vector<Edge<Weight>>();
 
         visit(0);
+        while (!pq.isEmpty()) {
+            Edge<Weight> e = pq.extractMin();
+            if (marked[e.v()] == marked[e.w()]) {
+                continue;
+            }
+
+            mst.add(e);
+            if (!marked[e.v()]) {
+                visit(e.v());
+            } else {
+                visit(e.w());
+            }
+        }
+
+        mstWeight = mst.elementAt(0).wt();
+        for (int i = 1; i < mst.size(); i++) {
+            mstWeight = mstWeight.doubleValue() + mst.elementAt(i).wt().doubleValue();
+        }
     }
 
     private void visit(int v) {
