bd258e200c48f3058d5bbfc8e5fc1e55240e4078
Lix <xiao.liunit@gmail.com>
2017-09-18 06:59:47 +0800

    size and construction function

--- Java/LeetCode/src/PlayWithAlgorithms/MinHeap.java
+++ Java/LeetCode/src/PlayWithAlgorithms/MinHeap.java
@@ -3,5 +3,22 @@ package PlayWithAlgorithms;
 /**
  * Created by Lix on 2017/9/18.
  */
-public class MinHeap {
+public class MinHeap<Item extends Comparable> {
+    private Item[] data;
+    private int count;
+    private int capacity;
+
+    public MinHeap(int capacity) {
+        data = (Item[]) new Comparable[capacity + 1];
+        this.count = 0;
+        this.capacity = capacity;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
 }
