cec1567d77301314c94dc1ec420a13604bcaea96
Lix <xiao.liunit@gmail.com>
2017-09-17 21:01:50 +0800

    finsh sparseWeightedGraph

--- Java/LeetCode/src/PlayWithAlgorithms/SparseWeightedGraph.java
+++ Java/LeetCode/src/PlayWithAlgorithms/SparseWeightedGraph.java
@@ -5,22 +5,22 @@ import java.util.Vector;
 /**
  * Created by Lix on 2017/9/17.
  */
-public class SparseWeightedGraph {
+public class SparseWeightedGraph<Weight extends Number & Comparable> implements WeightedGraph {
     private int n; // 节点数
     private int m; // 边数
     private boolean directed; // 是否为有向图
-    private Vector<Integer>[] g; // 图的具体数据
+    private Vector<Edge<Weight>>[] g; // 图的具体数据
 
     // 构建函数
-    public SparseGraph(int n, boolean directed) {
+    public SparseWeightedGraph(int n, boolean directed) {
         assert n >= 0;
         this.n = n;
         this.m = 0; // 初始化没有任何边
         this.directed = directed;
         // g初始化为n个空的vector,表示每一个g[i]都为空，即没有任何边
-        g = (Vector<Integer>[])new Vector[n];
+        g = (Vector<Edge<Weight>>[])new Vector[n];
         for (int i = 0; i < n; i++) {
-            g[i] = new Vector<Integer>();
+            g[i] = new Vector<Edge<Weight>>();
         }
     }
 
@@ -28,12 +28,12 @@ public class SparseWeightedGraph {
     public int E() { return m; } // 返回边的个数
 
     // 向图中添加一个边
-    public void addEdge(int v, int w) {
-        assert v >= 0 && v < n;
-        assert w >= 0 && w < n;
-        g[v].add(w);
-        if (v != w && !directed) {
-            g[w].add(v);
+    public void addEdge(Edge e) {
+        assert e.v() >= 0 && e.v() < n;
+        assert e.w() >= 0 && e.w() < n;
+        g[e.v()].add(new Edge(e));
+        if (e.v() != e.w() && !directed) {
+            g[e.w()].add(new Edge(e.w(), e.v(), e.wt()));
         }
 
         m++;
@@ -45,7 +45,7 @@ public class SparseWeightedGraph {
         assert w >= 0 && w < n;
 
         for (int i = 0; i < g[v].size(); i++) {
-            if (g[v].elementAt(i) == w) {
+            if (g[v].elementAt(i).other(v) == w) {
                 return true;
             }
         }
@@ -59,13 +59,14 @@ public class SparseWeightedGraph {
         for (int i = 0; i < n; i++) {
             System.out.print("vertex " + i + ":\t");
             for (int j = 0; j < g[i].size(); j++) {
-                System.out.print(g[i].elementAt(j) + "\t");
+                Edge e = g[i].elementAt(j);
+                System.out.print("( to:" + e.other(i) + ", wt:" + e.wt() + ")\t");
             }
             System.out.println();
         }
     }
 
-    public Iterable<Integer> adj(int v) {
+    public Iterable<Edge<Weight>> adj(int v) {
         assert( v >= 0 && v < n );
         return g[v];
     }
