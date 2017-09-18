78431b8b2810ce6b811c1defff9cffbca6551b51
Lix <xiao.liunit@gmail.com>
2017-09-18 09:55:20 +0800

    construction for indexmaxheap

--- Java/LeetCode/src/PlayWithAlgorithms/IndexMinHeap.java
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMinHeap.java
@@ -10,4 +10,16 @@ public class IndexMinHeap<Item extends Comparable> {
 
     private int count;
     private int capacity;
+
+    IndexMinHeap(int capacity) {
+        data = (Item[]) new Comparable[capacity + 1];
+        indexes = new int[capacity + 1];
+        reverse = new int[capacity + 1];
+
+        for (int i = 0; i <= capacity; i++) {
+            reverse[i] = -1;
+        }
+        count = 0;
+        this.capacity = capacity;
+    }
 }
