ea7e766cbdaee07157fa111237326d980f12500b
Lix <xiao.liunit@gmail.com>
2017-09-19 07:41:36 +0800

    construction for dijkstra

--- C++/Dijkstra.h
+++ C++/Dijkstra.h
@@ -6,6 +6,29 @@
 
 using namespace std;
 
+template <typename Graph, typename Weight>
 class Dijkstra {
+private:
+    Graph &G;
+    int s;
+    Weight *distTo;
+    bool *marked;
+    vector<Edge<Weight> *> from;
 
+public:
+    Dijkstra(Graph &graph, int s):G(graph) {
+        assert( s >= 0 && s < G.V() );
+        this->s = s;
+        distTo = new Weight[G.V()];
+        marked = new bool[G.V()];
+        for (int i = 0; i < G.V(); i++) {
+            distTo[i] = Weight();
+            marked[i] = false;
+            from.push_back(NULL);
+        }
+
+        IndexMinHeap<Weight> ipq(G.V());
+
+        //dijkstra
+    }
 };
