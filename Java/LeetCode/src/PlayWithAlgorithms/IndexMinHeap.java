0971a32d5b353eb4668adf6468dc7d7e98a10c47
Lix <xiao.liunit@gmail.com>
2017-09-18 09:42:20 +0800

    create IndexMinHeap class

---
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMinHeap.java
@@ -0,0 +1,13 @@
+package PlayWithAlgorithms;
+
+/**
+ * Created by Lix on 2017/9/18.
+ */
+public class IndexMinHeap<Item extends Comparable> {
+    private Item[] data;
+    private int[] indexes;
+    private int[] reverse;
+
+    private int count;
+    private int capacity;
+}
