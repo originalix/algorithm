5f1ba743102edf45346fc0838413dffd6bf2a4e8
Lix <xiao.liunit@gmail.com>
2017-09-19 10:36:15 +0800

    add comment

--- Java/LeetCode/src/PlayWithAlgorithms/IndexMinHeap.java
+++ Java/LeetCode/src/PlayWithAlgorithms/IndexMinHeap.java
@@ -3,14 +3,16 @@ package PlayWithAlgorithms;
 /**
  * Created by Lix on 2017/9/18.
  */
-public class IndexMinHeap<Item extends Comparable> {
-    private Item[] data;
-    private int[] indexes;
-    private int[] reverse;
 
+// 最小索引堆
+public class IndexMinHeap<Item extends Comparable> {
+    private Item[] data;    // 最小索引堆中的数据
+    private int[] indexes;  // 最小索引堆中的索引，indexes[x] = i 表示索引i在x的位置
+    private int[] reverse;  // 最小索引堆中的反向索引，reverse[i] = x 表示索引i在x的位置
     private int count;
     private int capacity;
 
+    // 构造函数，构造一个空堆，可容纳capacity个元素
     public IndexMinHeap(int capacity) {
         data = (Item[]) new Comparable[capacity + 1];
         indexes = new int[capacity + 1];
@@ -23,18 +25,23 @@ public class IndexMinHeap<Item extends Comparable> {
         this.capacity = capacity;
     }
 
+    // 返回索引堆中的个数
     public int size() {
         return count;
     }
 
+    // 返回一个布尔值，表示索引堆中是否为空
     public boolean isEmpty() {
         return count == 0;
     }
 
+    // 向最小堆中插入一个新的元素，新元素的索引为i，元素为item
+    // 传入的i对用户而言，是从0索引的
     public void insert(int index, Item item) {
         assert (count + 1 <= capacity);
         assert (index + 1 >= 1 && index + 1 <= capacity);
 
+        // 在插入一个新元素前，还需要保证索引i所在的位置是没有元素的
         assert !contain(index);
 
         index += 1;
@@ -46,6 +53,7 @@ public class IndexMinHeap<Item extends Comparable> {
         shiftUp(count);
     }
 
+    // 从最小索引堆中取出堆顶元素，即索引堆中所存储的最小数据
     public Item extractMin() {
         assert count > 0;
 
@@ -58,6 +66,7 @@ public class IndexMinHeap<Item extends Comparable> {
         return ret;
     }
 
+    // 从最小索引堆中取出堆顶元素的索引
     public int extractMinIndex() {
         assert count > 0;
         int ret = indexes[1] - 1;
@@ -68,35 +77,47 @@ public class IndexMinHeap<Item extends Comparable> {
         return ret;
     }
 
+    // 获取最小索引堆中的堆顶元素
     public Item getMin() {
         assert count > 0;
         return data[indexes[1]];
     }
 
+    // 获取最小索引堆中的堆顶元素的索引
     public int getMinIndex() {
         assert count > 0;
         return indexes[1] - 1;
     }
 
+    // 看索引i所在的位置是否存在元素
     private boolean contain(int i) {
         assert i + 1 >= 1 && i + 1 <= capacity;
         return reverse[i + 1] != 0;
     }
 
+    // 获取最小索引堆中索引为i的元素
     public Item getItem(int i) {
         assert contain(i);
         return data[i+1];
     }
 
+    // 将最小索引堆中索引为i的元素修改为newItem
     public void change(int i, Item newItem) {
         assert contain(i);
         i += 1;
         data[i] = newItem;
 
+        // 有了 reverse 之后,
+        // 我们可以非常简单的通过reverse直接定位索引i在indexes中的位置
         shiftUp(reverse[i]);
         shiftDown(reverse[i]);
     }
 
+    //********************
+    //* 最小索引堆核心辅助函数
+    //********************
+
+    // 索引堆中, 数据之间的比较根据data的大小进行比较, 但实际操作的是索引
     private void shiftUp(int k) {
         while (k > 1 && ( data[indexes[k/2]].compareTo( data[indexes[k]] ) > 0 )) {
             swapIndexes(k, k/2);
@@ -104,6 +125,7 @@ public class IndexMinHeap<Item extends Comparable> {
         }
     }
 
+    // 索引堆中, 数据之间的比较根据data的大小进行比较, 但实际操作的是索引
     private void shiftDown(int k) {
         while (2*k <= count) {
             int j = 2*k;
@@ -118,6 +140,9 @@ public class IndexMinHeap<Item extends Comparable> {
         }
     }
 
+    // 交换索引堆中的索引i和j
+    // 由于有了反向索引reverse数组，
+    // indexes数组发生改变以后， 相应的就需要维护reverse数组
     private void swapIndexes(int i, int j) {
         int t = indexes[i];
         indexes[i] = indexes[j];
