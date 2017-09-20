58dc01cdac6c0a4875ece328d25124faf22c48bb
Lix <xiao.liunit@gmail.com>
2017-09-20 09:30:25 +0800

    construction for bellmanford

--- C++/BellmanFord.h
+++ C++/BellmanFord.h
@@ -7,5 +7,22 @@ using namespace std;
 
 template<typename Graph, typename Weight>
 class BellmanFord {
+private:
+    Graph &G;
+    int s;
+    Weight* distTo;
+    vector<Edge<Weight>*> from;
+
+    bool hasNegativeCycle;
+
+public:
+    BellmanFord(Graph &graph, int s):G(graph) {
+
+    }
+
+    ~BellmanFord() {
+        delete[] distTo;
+        delete from[s];
+    }
 
 };
