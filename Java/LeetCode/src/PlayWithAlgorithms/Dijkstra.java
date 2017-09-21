7e02f90519823dbc331ff3f0fbf1d19e0822582f
Lix <xiao.liunit@gmail.com>
2017-09-21 16:55:42 +0800

    shortestPath function

--- Java/LeetCode/src/PlayWithAlgorithms/Dijkstra.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Dijkstra.java
@@ -1,5 +1,6 @@
 package PlayWithAlgorithms;
 
+import java.util.Stack;
 import java.util.Vector;
 
 /**
@@ -18,8 +19,30 @@ public class Dijkstra<Weight extends Number & Comparable> {
         return distTo[w];
     }
 
-    public boolean hasPathTo(int w) {
+    private boolean hasPathTo(int w) {
         assert w >= 0 && w < G.V();
         return marked[w];
+    }
+
+    private Vector<Edge<Weight>> shortestPath(int w) {
+        assert w >= 0 && w < G.V();
+        assert hasPathTo(w);
+
+        Stack<Edge<Weight>> s = new Stack<Edge<Weight>>();
+        Edge<Weight> e = from[w];
+        while (e.v() != this.s) {
+            s.push(e);
+            e = from[e.v()];
+        }
+
+        s.push(e);
+
+        Vector<Edge<Weight>> res = new Vector<Edge<Weight>>();
+        while (!s.isEmpty()) {
+            e = s.pop();
+            res.add(e);
+        }
+
+        return res;
     }
 }
