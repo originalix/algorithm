371513b8abd430184ecfda96c38b09fcbada0674
Lix <xiao.liunit@gmail.com>
2017-09-21 16:47:29 +0800

    Property for Dijkstra

--- Java/LeetCode/src/PlayWithAlgorithms/Dijkstra.java
+++ Java/LeetCode/src/PlayWithAlgorithms/Dijkstra.java
@@ -1,7 +1,14 @@
 package PlayWithAlgorithms;
 
+import java.util.Vector;
+
 /**
  * Created by Lix on 2017/9/21.
  */
-public class Dijkstra {
+public class Dijkstra<Weight extends Number & Comparable> {
+    private WeightedGraph G;
+    private int s;
+    private Number[] distTo;
+    private boolean[] marked;
+    private Edge<Weight>[] from;
 }
