74eca56b26e46d735b9074bb4ad4d67e7f942927
Lix <xiao.liunit@gmail.com>
2017-09-29 17:34:54 +0800

    test remove element case

--- Java/LeetCode/src/LeetCode/RemoveElement_27.java
+++ Java/LeetCode/src/LeetCode/RemoveElement_27.java
@@ -1,7 +1,21 @@
 package LeetCode;
 
+import javafx.scene.shape.VLineTo;
+
 /**
  * Created by Lix on 2017/9/29.
  */
 public class RemoveElement_27 {
+    public int removeElement(int[] nums, int val) {
+        int elementCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                elementCount += 1;
+            }
+        }
+
+        if (elementCount == 0) {
+
+        }
+    }
 }
