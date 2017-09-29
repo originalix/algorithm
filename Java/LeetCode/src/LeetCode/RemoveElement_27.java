eb89100947bdea4068999c950236310db431e683
Lix <xiao.liunit@gmail.com>
2017-09-29 20:14:03 +0800

    try to resolve #27

--- Java/LeetCode/src/LeetCode/RemoveElement_27.java
+++ Java/LeetCode/src/LeetCode/RemoveElement_27.java
@@ -6,16 +6,44 @@ import javafx.scene.shape.VLineTo;
  * Created by Lix on 2017/9/29.
  */
 public class RemoveElement_27 {
-    public int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) {
         int elementCount = 0;
+        int swapIndex = nums.length - 1;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
-                elementCount += 1;
+            if (nums[i] == val && swapIndex >= 0) {
+                if (nums[swapIndex] == val) {
+                    swapIndex -= 1;
+                }
+                if (swapIndex >= 0) {
+                    swap(nums, i, swapIndex);
+                    elementCount += 1;
+                    swapIndex -= 1;
+                }
             }
         }
 
         if (elementCount == 0) {
-
+            return nums.length;
         }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("now array["+ i +"] is like : " + nums[i]);
+        }
+        return nums.length - elementCount;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int result = removeElement(nums, val);
+        System.out.println("result is : " + result);
     }
 }
