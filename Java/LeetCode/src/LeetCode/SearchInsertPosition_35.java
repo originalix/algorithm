8dffd7e47c168be12c634accee998f16837f3d64
Lix <xiao.liunit@gmail.com>
2017-10-01 20:06:02 +0800

    create searchInsertPosition

---
+++ Java/LeetCode/src/LeetCode/SearchInsertPosition_35.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+/**
+ * Created by Lix on 2017/10/1.
+ */
+
+public class SearchInsertPosition_35 {
+    public static int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
