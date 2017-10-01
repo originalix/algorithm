95ffa399258ffa77b4170ac956d00fdc87b7de5e
Lix <xiao.liunit@gmail.com>
2017-10-01 20:19:45 +0800

    write #35

--- Java/LeetCode/src/LeetCode/SearchInsertPosition_35.java
+++ Java/LeetCode/src/LeetCode/SearchInsertPosition_35.java
@@ -6,6 +6,23 @@ package LeetCode;
 
 public class SearchInsertPosition_35 {
     public static int searchInsert(int[] nums, int target) {
-        return 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return mid;
     }
 }
+
+// nums有序
+// 二分法查找target
+// 若找不到 return index
