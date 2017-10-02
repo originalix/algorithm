93e140056dbb6ecec360743ffc4d57fd2a61e4ab
Lix <xiao.liunit@gmail.com>
2017-10-02 16:37:35 +0800

    improve #35

--- Java/LeetCode/src/LeetCode/SearchInsertPosition_35.java
+++ Java/LeetCode/src/LeetCode/SearchInsertPosition_35.java
@@ -8,22 +8,18 @@ public class SearchInsertPosition_35 {
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid = 0;
         while (left <= right) {
-            mid = (left + right) / 2;
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
-        if (nums[mid] < target) {
-            mid += 1;
-        }
-        return mid;
+        return left;
     }
 
     public static void main(String[] args) {
