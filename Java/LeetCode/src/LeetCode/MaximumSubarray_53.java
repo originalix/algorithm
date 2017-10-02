259910221d9ee544d8bd94fef6276f7df04d4342
Lix <xiao.liunit@gmail.com>
2017-10-02 17:00:30 +0800

    test max subarray

--- Java/LeetCode/src/LeetCode/MaximumSubarray_53.java
+++ Java/LeetCode/src/LeetCode/MaximumSubarray_53.java
@@ -4,8 +4,24 @@ package LeetCode;
  * Created by Lix on 2017/10/2.
  */
 public class MaximumSubarray_53 {
-    public int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,-2,-3,-4,-5,-6};
+        int maxSum = maxSubArray(nums);
+        System.out.println("max sum is : " + maxSum);
     }
 }
