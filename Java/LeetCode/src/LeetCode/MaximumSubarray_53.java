d9add38d1aec0fee97e8c9f23bfe627d4dea9406
Lix <xiao.liunit@gmail.com>
2017-10-04 19:57:53 +0800

    create plus one #66

--- Java/LeetCode/src/LeetCode/MaximumSubarray_53.java
+++ Java/LeetCode/src/LeetCode/MaximumSubarray_53.java
@@ -18,4 +18,10 @@ public class MaximumSubarray_53 {
         }
         return maxSum;
     }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,2,-3,-4,-5,-6};
+        int maxSum = maxSubArray(nums);
+        System.out.println("max sum is : " + maxSum);
+    }
 }
