491859b7fee40cecf17ac7131492799e976bf5e6
Lix <xiao.liunit@gmail.com>
2017-10-05 17:38:00 +0800

    main function

--- Java/LeetCode/src/LeetCode/PlusOne_66.java
+++ Java/LeetCode/src/LeetCode/PlusOne_66.java
@@ -4,4 +4,12 @@ package LeetCode;
  * Created by Lix on 2017/10/4.
  */
 public class PlusOne_66 {
+    public static int[] plusOne(int[] digits) {
+        return new int[]{1, 2, 3};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = plusOne(new int[]{1});
+        System.out.println(nums);
+    }
 }
