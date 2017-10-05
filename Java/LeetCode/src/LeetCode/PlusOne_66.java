07015ce58163b103e43856fa396d7830a18592ff
Lix <xiao.liunit@gmail.com>
2017-10-05 20:49:04 +0800

    test plus all

--- Java/LeetCode/src/LeetCode/PlusOne_66.java
+++ Java/LeetCode/src/LeetCode/PlusOne_66.java
@@ -8,8 +8,19 @@ public class PlusOne_66 {
         return new int[]{1, 2, 3};
     }
 
+    private static void plusAll() {
+//        int[] nums = new int[]{1, 0, 2, 4};
+        int nums = 1024;
+        for (int i = 0; i < 4; i++) {
+            int a = nums % (i * 10);
+            System.out.println("now a = " + a);
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = plusOne(new int[]{1});
         System.out.println(nums);
+
+        plusAll();
     }
 }
