bdfa04469f26fb5285136ad1196d41bb47c97e15
Lix <xiao.liunit@gmail.com>
2017-10-07 17:04:07 +0800

    write code for plusone

--- Java/LeetCode/src/LeetCode/PlusOne_66.java
+++ Java/LeetCode/src/LeetCode/PlusOne_66.java
@@ -5,12 +5,25 @@ package LeetCode;
  */
 public class PlusOne_66 {
     public static int[] plusOne(int[] digits) {
-        return new int[]{1, 2, 3};
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[n-1] = 0;
+        }
+
+        int[] newNumber = new int[n+1];
+        newNumber[0] = 1;
+        return  newNumber;
     }
 
     public static void main(String[] args) {
-        int[] nums = plusOne(new int[]{1});
-        System.out.println(nums);
-
+        int[] nums = plusOne(new int[]{1, 2, 3});
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
