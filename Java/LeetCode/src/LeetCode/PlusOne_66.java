39c75771f404fcfcc6b243faf1cbef31f59057e5
Lix <xiao.liunit@gmail.com>
2017-10-07 17:01:23 +0800

    delete  test code

--- Java/LeetCode/src/LeetCode/PlusOne_66.java
+++ Java/LeetCode/src/LeetCode/PlusOne_66.java
@@ -8,40 +8,9 @@ public class PlusOne_66 {
         return new int[]{1, 2, 3};
     }
 
-    private static void plusAll() {
-//        int[] nums = new int[]{1, 0, 2, 4};
-        int nums = 1024;
-
-        // 1
-        // 0
-        // 2
-        // 4
-//        for (int i = 1; i <= 4; i++) {
-//            int a = nums % 10000;
-//            System.out.println("now a = " + a);
-//        }
-
-        int a = nums / 1000;
-        System.out.println("now a = " + a);
-        int b = nums / 100;
-        System.out.println("now b = " + b);
-        int c = nums / 10;
-        System.out.println("now c = " + c);
-        int d = nums / 1;
-        System.out.println("now d = " + d);
-
-        int e = b % a;
-        int f = c % b;
-        int g = d % c;
-        System.out.println("now e = " + e);
-        System.out.println("now f = " + f);
-        System.out.println("now g = " + g);
-    }
-
     public static void main(String[] args) {
         int[] nums = plusOne(new int[]{1});
         System.out.println(nums);
 
-        plusAll();
     }
 }
