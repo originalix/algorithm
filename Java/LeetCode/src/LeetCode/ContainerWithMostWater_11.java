d9dd9f55e5987a806a34b406526f3435b898eaf1
Lix <xiao.liunit@gmail.com>
2017-10-10 06:59:52 +0800

    #11 thinking

--- Java/LeetCode/src/LeetCode/ContainerWithMostWater_11.java
+++ Java/LeetCode/src/LeetCode/ContainerWithMostWater_11.java
@@ -5,6 +5,11 @@ package LeetCode;
  */
 public class ContainerWithMostWater_11 {
 
+    // 思路分析：
+    // 构建成坐标点
+    // 使用O( logN^2 )的时间复杂度 暴力的对比 记录最大的面积
+    // 返回最大的面积
+    // 注意点，构建坐标，存水的面积计算
     public static int maxArea(int[] height) {
 
         return 0;
