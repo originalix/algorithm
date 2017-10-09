20d7261a2b8550e2a75018f821d1c326da2fb0d5
Lix <xiao.liunit@gmail.com>
2017-10-10 07:12:57 +0800

    finish maxArea

--- Java/LeetCode/src/LeetCode/ContainerWithMostWater_11.java
+++ Java/LeetCode/src/LeetCode/ContainerWithMostWater_11.java
@@ -11,17 +11,30 @@ public class ContainerWithMostWater_11 {
     // 返回最大的面积
     // 注意点，构建坐标，存水的面积计算
     public static int maxArea(int[] height) {
+        int maxArea = -999;
         for (int i = 0; i < height.length; i++) {
 //            System.out.println("第" + i + "个坐标: " + "(" + i + "," + height[i] + ")");
             for (int j = 0; j < height.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                int Width = i > j ? i- j : j - i;
+                int areaHeight = height[i] < height[j] ? height[i] : height[j];
+
                 System.out.println("当前比较的两个坐标: " + "(" + i + "," + height[i] + ") -> " + "(" + j + "," + height[j] + ")");
+                System.out.println("当前面积的宽: " + Width + " 高 : " + areaHeight);
+                int area = Width * areaHeight;
+                if (area > maxArea) {
+                    maxArea = area;
+                }
             }
         }
-        return 0;
+        return maxArea;
     }
 
     public static void main(String[] args) {
         int[] height = new int[]{1, 3, 2};
         int a = maxArea(height);
+        System.out.println("最大面积为: " + a);
     }
 }
