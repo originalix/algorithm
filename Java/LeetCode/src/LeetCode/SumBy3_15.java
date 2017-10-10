9bbaff2ede3af50580e640742a55a46d995377b1
Lix <xiao.liunit@gmail.com>
2017-10-11 07:09:10 +0800

    delete bug code

--- Java/LeetCode/src/LeetCode/SumBy3_15.java
+++ Java/LeetCode/src/LeetCode/SumBy3_15.java
@@ -1,6 +1,9 @@
 package LeetCode;
 
+import com.sun.tools.hat.internal.util.ArraySorter;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,52 +12,14 @@ import java.util.List;
 public class SumBy3_15 {
     // 思路：暴力解法
     public static List<List<Integer>> threeSum(int[] nums) {
-        int a = 0;
-        int b = 1;
-        int c = 2;
+        Arrays.sort(nums);
+        System.out.println("nums = " + nums);
 
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-
-//                    System.out.println("now a = " + nums[i] + ", b = " + nums[j] + ", c = " + nums[k]);
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-
-                        List<Integer> answer = new ArrayList<Integer>();
-                        answer.add(nums[i]);
-                        answer.add(nums[j]);
-                        answer.add(nums[k]);
-
-                        System.out.println("all answer : " + answer);
-
-                        boolean isRepeat = false;
-                        for (int e = 0; e < result.size(); e++) {
-                            List<Integer> item = result.get(e);
-                            boolean test = item.indexOf(nums[i]) == item.indexOf(nums[j]) && item.indexOf(nums[i]) == item.indexOf(nums[k]);
-
-                            if ( item.contains(nums[i]) && item.contains(nums[j]) && item.contains(nums[k])) {
-                                isRepeat = true;
-                                if (test) {
-                                    isRepeat = false;
-                                }
-                            }
-                        }
-
-                        if (!isRepeat) {
-                            result.add(answer);
-                        }
-                    }
-                }
-            }
-        }
-
-        return result;
+        return null;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,0,0,0};
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = threeSum(nums);
         for (int i = 0; i < result.size(); i++) {
             System.out.println(result.get(i));
