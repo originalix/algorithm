5d1f6d6632cdb7c3e8203ed236dcd90c089d6eb7
Lix <xiao.liunit@gmail.com>
2017-10-11 07:20:09 +0800

    finish #15

--- Java/LeetCode/src/LeetCode/SumBy3_15.java
+++ Java/LeetCode/src/LeetCode/SumBy3_15.java
@@ -4,18 +4,40 @@ import com.sun.tools.hat.internal.util.ArraySorter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by Lix on 2017/10/10.
  */
 public class SumBy3_15 {
-    // 思路：暴力解法
+    // 思路：排序之后，对撞指针
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        System.out.println("nums = " + nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int lo = i+1, hi = nums.length - 1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo+1]) {
+                            lo++;
+                        }
+                        while (lo < hi && nums[hi] == nums[hi-1]) {
+                            hi--;
+                        }
+                        lo++; hi--;
+                    } else if (nums[lo] + nums[hi] < sum) {
+                        lo++;
+                    } else {
+                        hi--;
+                    }
+                }
+            }
+        }
 
-        return null;
+        return res;
     }
 
     public static void main(String[] args) {
