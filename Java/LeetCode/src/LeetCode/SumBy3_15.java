cd1040150b610c56d4942a61d7c64f71c16514ca
Lix <xiao.liunit@gmail.com>
2017-10-10 20:41:28 +0800

    debug log for threeSum

--- Java/LeetCode/src/LeetCode/SumBy3_15.java
+++ Java/LeetCode/src/LeetCode/SumBy3_15.java
@@ -15,9 +15,11 @@ public class SumBy3_15 {
 
         List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-        for (int i = a; i < nums.length; i++) {
-            for (int j = b; j < nums.length; j++) {
-                for (int k = c; k < nums.length; k++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+
+//                    System.out.println("now a = " + nums[i] + ", b = " + nums[j] + ", c = " + nums[k]);
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         List<Integer> answer = new ArrayList<Integer>();
                         answer.add(nums[i]);
