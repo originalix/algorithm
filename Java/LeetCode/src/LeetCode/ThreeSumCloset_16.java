45b6b5ac4b62b14ae9a13a4d22260290201e7631
Lix <xiao.liunit@gmail.com>
2017-10-11 20:59:31 +0800

    test three sum closest

--- Java/LeetCode/src/LeetCode/ThreeSumCloset_16.java
+++ Java/LeetCode/src/LeetCode/ThreeSumCloset_16.java
@@ -1,7 +1,35 @@
 package LeetCode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Lix on 2017/10/11.
  */
 public class ThreeSumCloset_16 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int cloestRes = 999999999;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int lo = i+1, hi = nums.length - 1, sum = target - nums[i];
+            while (lo < hi) {
+                if ( Math.abs(nums[lo] + nums[hi] + nums[i] - target) < cloestRes ) {
+                    cloestRes = nums[lo] + nums[hi] + nums[i];
+                }
+                if (nums[lo] + nums[hi] == sum) {
+                    return target;
+                } else if (nums[lo] + nums[hi] < sum) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+            }
+        }
+
+        return cloestRes;
+    }
 }
