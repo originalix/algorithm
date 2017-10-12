f5c2ca70f5143a380da79c17de493e84accc1d26
Lix <xiao.liunit@gmail.com>
2017-10-12 20:31:19 +0800

    four sum function

--- Java/LeetCode/src/LeetCode/FourSum_18.java
+++ Java/LeetCode/src/LeetCode/FourSum_18.java
@@ -1,7 +1,18 @@
 package LeetCode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Lix on 2017/10/12.
  */
 public class FourSum_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+
+        return res;
+    }
 }
