edd35ea0cb53d7c2d97e3907befbc9745d9e8cd6
Lix <xiao.liunit@gmail.com>
2017-10-12 21:01:11 +0800

    test search sorted rotate array

--- Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
+++ Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
@@ -1,7 +1,57 @@
 package LeetCode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Lix on 2017/10/12.
  */
 public class SearchRotatedSortedArray_33 {
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if ( j != i+1 && nums[j - 1] == nums[j] ) {
+                    continue;
+                }
+
+                int lo = j + 1, hi = nums.length - 1;
+                while (lo < hi) {
+                    int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
+                    if (sum == target) {
+
+                        List<Integer> answer = new LinkedList<Integer>();
+                        answer.add(nums[i]);
+                        answer.add(nums[j]);
+                        answer.add(nums[lo]);
+                        answer.add(nums[hi]);
+                        res.add(answer);
+
+                        lo++; hi--;
+                        while (lo < hi && nums[lo] == nums[lo - 1]) {
+                            lo++;
+                        }
+
+                        while (lo < hi && nums[hi] == nums[hi + 1]) {
+                            hi--;
+                        }
+                    } else if (sum < target) {
+                        lo++;
+                    } else {
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
