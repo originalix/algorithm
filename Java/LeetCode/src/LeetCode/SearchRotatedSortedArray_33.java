bc917e4adb1c243d06d2deac759f29935c3a4d3e
Lix <xiao.liunit@gmail.com>
2017-10-13 07:23:35 +0800

    finish #33

--- Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
+++ Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
@@ -8,7 +8,7 @@ import java.util.List;
  * Created by Lix on 2017/10/12.
  */
 public class SearchRotatedSortedArray_33 {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int lo = 0, hi = nums.length - 1;
         while (lo < hi) {
             int mid = (lo + hi) / 2;
@@ -32,5 +32,11 @@ public class SearchRotatedSortedArray_33 {
         }
 
         return nums[lo] == target ? lo : -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int index = search(nums, 2);
+        System.out.println("index = " + index);
     }
 }
