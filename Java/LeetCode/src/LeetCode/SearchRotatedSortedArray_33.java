e1496f812754c062af95108b2921e78f8a49431f
Lix <xiao.liunit@gmail.com>
2017-10-13 06:56:41 +0800

    test search rotated sorted array

--- Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
+++ Java/LeetCode/src/LeetCode/SearchRotatedSortedArray_33.java
@@ -8,5 +8,14 @@ import java.util.List;
  * Created by Lix on 2017/10/12.
  */
 public class SearchRotatedSortedArray_33 {
+    public int search(int[] nums, int target) {
+        int resIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                resIndex = i;
+            }
+        }
 
+        return resIndex;
+    }
 }
