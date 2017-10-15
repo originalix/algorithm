182099d70e41c20e8ed29f5c80438f0d146c3251
Lix <xiao.liunit@gmail.com>
2017-10-15 20:26:50 +0800

    think for #31

--- Java/LeetCode/src/LeetCode/NextPermutation_31.java
+++ Java/LeetCode/src/LeetCode/NextPermutation_31.java
@@ -4,4 +4,10 @@ package LeetCode;
  * Created by Lix on 2017/10/14.
  */
 public class NextPermutation_31 {
+    // 思路：查找最后的元素 比较大小 往前移
+    // 若都不行 sort之后 从小到大 输出
+
+    public void nextPermutation(int[] nums) {
+
+    }
 }
