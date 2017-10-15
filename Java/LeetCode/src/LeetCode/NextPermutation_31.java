a11c5a52711210a67a8b1bd7e1cee08f72451761
Lix <xiao.liunit@gmail.com>
2017-10-15 20:33:34 +0800

    test next Permutation

--- Java/LeetCode/src/LeetCode/NextPermutation_31.java
+++ Java/LeetCode/src/LeetCode/NextPermutation_31.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 /**
  * Created by Lix on 2017/10/14.
  */
@@ -7,7 +9,22 @@ public class NextPermutation_31 {
     // 思路：查找最后的元素 比较大小 往前移
     // 若都不行 sort之后 从小到大 输出
 
-    public void nextPermutation(int[] nums) {
+    public static void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums.length - 2 >= 0) {
+                if (nums[i] > nums[i-1]) {
+                    swap(nums, i, i-1);
+                    return;
+                }
+            }
+        }
 
+        Arrays.sort(nums);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
