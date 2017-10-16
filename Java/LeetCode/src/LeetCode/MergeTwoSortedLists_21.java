e4dae018791a0639b053d2ca199f6c0bea3db4fb
Lix <xiao.liunit@gmail.com>
2017-10-16 21:06:00 +0800

    create merge two sorted lists

---
+++ Java/LeetCode/src/LeetCode/MergeTwoSortedLists_21.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+/**
+ * Created by Lix on 2017/10/16.
+ */
+public class MergeTwoSortedLists_21 {
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+}
