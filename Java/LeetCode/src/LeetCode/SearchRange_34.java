a432a4fc9d30f10ac5a04bdedb59ffe269744e8e
Lix <xiao.liunit@gmail.com>
2017-10-16 20:59:39 +0800

    great function for search range

--- Java/LeetCode/src/LeetCode/SearchRange_34.java
+++ Java/LeetCode/src/LeetCode/SearchRange_34.java
@@ -16,7 +16,23 @@ public class SearchRange_34 {
     }
 
     public void searchRange(int[] A, int target, int left, int right, int[] range) {
+        if (left > right) return;
+        int mid = left + (right - left) / 2;
+        if (A[mid] == target) {
+            if (mid < range[0]) {
+                range[0] = mid;
+                searchRange(A, target, left, mid - 1, range);
+            }
 
+            if (mid > range[1]) {
+                range[1] = mid;
+                searchRange(A, target, mid + 1, right, range);
+            }
+        } else if (A[mid] < target) {
+            searchRange(A, target, mid + 1, right, range);
+        } else {
+            searchRange(A, target, left, mid - 1, range);
+        }
     }
 
     public static void main(String[] args) {
