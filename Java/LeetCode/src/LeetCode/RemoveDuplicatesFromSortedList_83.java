2f606e1a1248cdc42aa53617fd11010ee480d218
Lix <xiao.liunit@gmail.com>
2017-10-17 07:25:00 +0800

    test remove duplicates from sorted list

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedList_83.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedList_83.java
@@ -7,4 +7,17 @@ public class RemoveDuplicatesFromSortedList_83 {
     public ListNode deleteDuplicates(ListNode head) {
 
     }
+
+    public ListNode deleteDuplicates(ListNode node, int target) {
+
+        if (node == null) return node;
+
+        if (node.val != target) {
+            return node;
+        }
+
+        node.next = deleteDuplicates(node.next, node.val);
+
+        return node;
+    }
 }
