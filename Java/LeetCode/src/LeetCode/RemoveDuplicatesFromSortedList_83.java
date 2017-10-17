b64cc31baaaee06e471948b8079a78b5a9da81da
Lix <xiao.liunit@gmail.com>
2017-10-17 13:55:54 +0800

    test remove duplicates element

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedList_83.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedList_83.java
@@ -1,23 +1,25 @@
 package LeetCode;
 
+import java.lang.annotation.Target;
+
 /**
  * Created by Lix on 2017/10/17.
  */
 public class RemoveDuplicatesFromSortedList_83 {
     public ListNode deleteDuplicates(ListNode head) {
-
+        if (head == null) return head;
+        head.next = deleteDuplicates(head.next, head.val);
+        return head;
     }
 
-    public ListNode deleteDuplicates(ListNode node, int target) {
+    public ListNode deleteDuplicates(ListNode node, int val) {
 
         if (node == null) return node;
-
-        if (node.val != target) {
+        if (node.next != null) {
+            node.next = deleteDuplicates(node.next, node.val);
             return node;
         }
 
-        node.next = deleteDuplicates(node.next, node.val);
-
-        return node;
+        if (node.val != val) return node;
     }
 }
