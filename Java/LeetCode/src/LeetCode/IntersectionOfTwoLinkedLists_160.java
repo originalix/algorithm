a374632cefe11202d9f5aa23a14fdb7320b87e08
Lix <xiao.liunit@gmail.com>
2017-10-17 20:23:35 +0800

    length function for listnode

--- Java/LeetCode/src/LeetCode/IntersectionOfTwoLinkedLists_160.java
+++ Java/LeetCode/src/LeetCode/IntersectionOfTwoLinkedLists_160.java
@@ -4,4 +4,17 @@ package LeetCode;
  * Created by Lix on 2017/10/17.
  */
 public class IntersectionOfTwoLinkedLists_160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    public int length(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            head = head.next;
+            length++;
+        }
+
+        return length;
+    }
 }
