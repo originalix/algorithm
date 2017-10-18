3bd423210eb39c7a79c5eeba1b546d03dd81c918
Lix <xiao.liunit@gmail.com>
2017-10-18 19:53:11 +0800

    test for #203

--- Java/LeetCode/src/LeetCode/RemoveLinkedListElements_203.java
+++ Java/LeetCode/src/LeetCode/RemoveLinkedListElements_203.java
@@ -4,4 +4,10 @@ package LeetCode;
  * Created by Lix on 2017/10/18.
  */
 public class RemoveLinkedListElements_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null || head.next == null) return head;
+        head = removeElements(head, val);
+        return head.val == val ? head.next : head;
+    }
+
 }
