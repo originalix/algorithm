d04a6bc5b4864c6a381bc94d22a1ade421309743
Lix <xiao.liunit@gmail.com>
2017-10-20 07:31:59 +0800

    test #234

--- Java/LeetCode/src/LeetCode/PalindromeLinkedList_234.java
+++ Java/LeetCode/src/LeetCode/PalindromeLinkedList_234.java
@@ -4,4 +4,26 @@ package LeetCode;
  * Created by Lix on 2017/10/20.
  */
 public class PalindromeLinkedList_234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode next = null;
+        ListNode pre = null;
+        ListNode temp = head;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        while (temp != null) {
+            if (temp.val != pre.val) {
+                return false;
+            }
+            temp = temp.next;
+        }
+
+        return true;
+
+    }
 }
