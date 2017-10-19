11e18cfedee328ca997b25e6f69236e8a545554b
Lix <xiao.liunit@gmail.com>
2017-10-19 20:58:18 +0800

    iteratively for reverse linked list

--- Java/LeetCode/src/LeetCode/ReverseLinkedList_206.java
+++ Java/LeetCode/src/LeetCode/ReverseLinkedList_206.java
@@ -1,12 +1,23 @@
 package LeetCode;
 
+import java.util.Stack;
+
 /**
  * Created by Lix on 2017/10/19.
  */
 public class ReverseLinkedList_206 {
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        head = reverseList(head.next);
-        return head;
+
+        ListNode next = null;
+        ListNode pre = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
     }
 }
