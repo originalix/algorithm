95227b2a6a97167f87a630fd210c094da3cd202d
Lix <xiao.liunit@gmail.com>
2017-10-19 20:26:33 +0800

    test #206

--- Java/LeetCode/src/LeetCode/ReverseLinkedList_206.java
+++ Java/LeetCode/src/LeetCode/ReverseLinkedList_206.java
@@ -4,4 +4,9 @@ package LeetCode;
  * Created by Lix on 2017/10/19.
  */
 public class ReverseLinkedList_206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        head = reverseList(head.next);
+        return head;
+    }
 }
