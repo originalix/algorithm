b0373c0cdc3a8a632f7be93c0ed08e3c9d48be51
Lix <xiao.liunit@gmail.com>
2017-10-20 13:49:36 +0800

    delete wrong code

--- Java/LeetCode/src/LeetCode/PalindromeLinkedList_234.java
+++ Java/LeetCode/src/LeetCode/PalindromeLinkedList_234.java
@@ -5,25 +5,6 @@ package LeetCode;
  */
 public class PalindromeLinkedList_234 {
     public boolean isPalindrome(ListNode head) {
-        ListNode next = null;
-        ListNode pre = null;
-        ListNode temp = head;
-
-        while (head != null) {
-            next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
-        }
-
-        while (temp != null) {
-            if (temp.val != pre.val) {
-                return false;
-            }
-            temp = temp.next;
-        }
-
-        return true;
 
     }
 }
