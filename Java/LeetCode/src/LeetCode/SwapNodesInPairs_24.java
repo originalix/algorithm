446a0510cc39b96c4cc4a063cf9e8975e0318ed4
Lix <xiao.liunit@gmail.com>
2017-10-24 20:30:19 +0800

    test swap pairs

--- Java/LeetCode/src/LeetCode/SwapNodesInPairs_24.java
+++ Java/LeetCode/src/LeetCode/SwapNodesInPairs_24.java
@@ -4,4 +4,23 @@ package LeetCode;
  * Created by Lix on 2017/10/24.
  */
 public class SwapNodesInPairs_24 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = null;
+        while (head != null) {
+            ListNode a = head;
+            ListNode b = a.next;
+            if (b == null) {
+                break;
+            }
+            ListNode T = b.next;
+            ListNode T1 = a;
+            a = b;
+            b = T1;
+            b.next = a;
+            a.next = T;
+            temp = b;
+            head = head.next.next;
+        }
+        return temp;
+    }
 }
