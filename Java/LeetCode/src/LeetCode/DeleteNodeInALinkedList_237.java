eed0aa0e7b2843001f4170dad189fd4f7391b499
Lix <xiao.liunit@gmail.com>
2017-10-24 15:11:48 +0800

    finish #237

--- Java/LeetCode/src/LeetCode/DeleteNodeInALinkedList_237.java
+++ Java/LeetCode/src/LeetCode/DeleteNodeInALinkedList_237.java
@@ -4,5 +4,12 @@ package LeetCode;
  * Created by Lix on 2017/10/24.
  */
 public class DeleteNodeInALinkedList_237 {
-
+    public void deleteNode(ListNode node) {
+        if (node.next == null) {
+            node = null;
+        } else {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
 }
