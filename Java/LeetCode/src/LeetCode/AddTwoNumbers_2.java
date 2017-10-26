99f08f3f302a67e725ea317b5c3efb60b035179b
Lix <xiao.liunit@gmail.com>
2017-10-26 15:45:19 +0800

    reverse function for #2

--- Java/LeetCode/src/LeetCode/AddTwoNumbers_2.java
+++ Java/LeetCode/src/LeetCode/AddTwoNumbers_2.java
@@ -3,5 +3,21 @@ package LeetCode;
 /**
  * Created by Lix on 2017/10/26.
  */
-public class AddTwoNumbers_2g {
+public class AddTwoNumbers_2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode pre = null;
+        ListNode next = null;
+        while (node != null) {
+            next = node.next;
+            node.next = pre;
+            pre = node;
+            node = next;
+        }
+
+        return node;
+    }
 }
