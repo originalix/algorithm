9742033be254b7a3fb90abf2e18dfc06d559a2d3
Lix <xiao.liunit@gmail.com>
2017-10-27 07:34:34 +0800

    solution for #19

--- Java/LeetCode/src/LeetCode/RemoveNthNodeFromEndOfList_19.java
+++ Java/LeetCode/src/LeetCode/RemoveNthNodeFromEndOfList_19.java
@@ -4,4 +4,33 @@ package LeetCode;
  * Created by Lix on 2017/10/27.
  */
 public class RemoveNthNodeFromEndOfList_19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode reverse = reverseListFor19(head);
+        ListNode node = reverse;
+
+        int index = 1;
+        while (reverse != null) {
+            if (index + 1 == n) {
+                reverse.next = reverse.next.next;
+            }
+
+            index += 1;
+            reverse = reverse.next;
+        }
+
+        ListNode res = reverseListFor19(node.next);
+        return res;
+    }
+
+    public ListNode reverseListFor19(ListNode head) {
+        ListNode next = null;
+        ListNode pre = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
 }
