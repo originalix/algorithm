af6c35ec83cfb02744c90ffcf7f4b119e0358ddd
Lix <xiao.liunit@gmail.com>
2017-10-27 07:38:15 +0800

    test #19

--- Java/LeetCode/src/LeetCode/RemoveNthNodeFromEndOfList_19.java
+++ Java/LeetCode/src/LeetCode/RemoveNthNodeFromEndOfList_19.java
@@ -33,4 +33,16 @@ public class RemoveNthNodeFromEndOfList_19 {
         }
         return pre;
     }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+
+        RemoveNthNodeFromEndOfList_19 obj = new RemoveNthNodeFromEndOfList_19();
+        ListNode res = obj.removeNthFromEnd(l1, 1);
+        System.out.println(res);
+    }
 }
