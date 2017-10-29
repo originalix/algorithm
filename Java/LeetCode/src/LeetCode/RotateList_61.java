53ce05d7a48c8355e493d8fc0f11188c6b86d58d
Lix <xiao.liunit@gmail.com>
2017-10-29 20:44:38 +0800

    write think about #61

--- Java/LeetCode/src/LeetCode/RotateList_61.java
+++ Java/LeetCode/src/LeetCode/RotateList_61.java
@@ -4,6 +4,16 @@ package LeetCode;
  * Created by Lix on 2017/10/28.
  */
 public class RotateList_61 {
+
+    /**
+     *
+     * 初步思路:
+     * 1、反转链表
+     * 2、寻找k的节点
+     * 3、反转k之前的节点
+     * 4、反转k之后的链表，并在找到k之后，k.next 置为null
+     * 5、拼接链表
+     */
     public ListNode rotateRight(ListNode head, int k) {
         return null;
     }
