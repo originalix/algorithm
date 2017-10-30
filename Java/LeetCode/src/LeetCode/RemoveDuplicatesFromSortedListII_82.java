56453226ffee06487905c7fd5af3b879aecf7cb3
Lix <xiao.liunit@gmail.com>
2017-10-31 07:22:18 +0800

    function for delete duplicates

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
@@ -3,5 +3,18 @@ package LeetCode;
 /**
  * Created by Lix on 2017/10/31.
  */
-public class RemoveDuplicatesFromSortedListII_82g {
+public class RemoveDuplicatesFromSortedListII_82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+        while (head.val == head.next.val) {
+            if (head.next.next != null) {
+                head = head.next.next;
+            } else {
+                return null;
+            }
+        }
+
+        head = deleteDuplicates(head.next);
+        return head;
+    }
 }
