2113249dd7223d52f0a1e9a3bbec9edcbc7476ad
Lix <xiao.liunit@gmail.com>
2017-10-31 15:53:05 +0800

    test #82

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
@@ -17,4 +17,18 @@ public class RemoveDuplicatesFromSortedListII_82 {
         head = deleteDuplicates(head.next);
         return head;
     }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(1);
+        ListNode l4 = new ListNode(2);
+        ListNode l5 = new ListNode(3);
+        l1.next = l2; l2.next = l3; l3.next = l4; l4.next = l5;
+
+        RemoveDuplicatesFromSortedListII_82 obj = new RemoveDuplicatesFromSortedListII_82();
+
+        ListNode res = obj.deleteDuplicates(l1);
+        System.out.println(res);
+    }
 }
