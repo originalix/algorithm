f0c23dc11349680ffb60ae50984e6d08d820afb6
Lix <xiao.liunit@gmail.com>
2017-10-31 16:44:55 +0800

    finish #82

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
@@ -5,26 +5,37 @@ package LeetCode;
  */
 public class RemoveDuplicatesFromSortedListII_82 {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return head;
-        while (head.val == head.next.val) {
-            if (head.next.next != null) {
-                head = head.next.next;
+        if (head == null || head.next == null) return head;
+        int oldValue = 99999999;
+        while (head.val == head.next.val || head.val == oldValue) {
+            if (head.next != null) {
+                oldValue = head.val;
+                head = head.next;
+                if (head.next == null && head.val == oldValue) {
+                    return null;
+                }
+                if (head.next == null) {
+                    break;
+                }
             } else {
                 return null;
             }
         }
 
-        head = deleteDuplicates(head.next);
+        head.next = deleteDuplicates(head.next);
         return head;
     }
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(1);
-        ListNode l3 = new ListNode(1);
-        ListNode l4 = new ListNode(2);
-        ListNode l5 = new ListNode(3);
-        l1.next = l2; l2.next = l3; l3.next = l4; l4.next = l5;
+//        ListNode l3 = new ListNode(1);
+//        ListNode l4 = new ListNode(2);
+//        ListNode l5 = new ListNode(3);
+        l1.next = l2;
+//        l2.next = l3;
+//        l3.next = l4;
+//        l4.next = l5;
 
         RemoveDuplicatesFromSortedListII_82 obj = new RemoveDuplicatesFromSortedListII_82();
 
