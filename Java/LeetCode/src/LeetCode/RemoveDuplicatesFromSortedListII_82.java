5d7e8b0185cfd5e5d048f636aae5f71a07c8c64d
Lix <xiao.liunit@gmail.com>
2017-11-01 09:32:43 +0800

    iteration for #82

--- Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
+++ Java/LeetCode/src/LeetCode/RemoveDuplicatesFromSortedListII_82.java
@@ -46,7 +46,27 @@ public class RemoveDuplicatesFromSortedListII_82 {
     }
 
     public ListNode deleteDuplicatesByIteration(ListNode head) {
-        return null;
+        if (head == null) return null;
+        ListNode FakeHead = new ListNode(0);
+        FakeHead.next = head;
+        ListNode pre = FakeHead;
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
+            }
+
+            if (pre.next == cur) {
+                pre = pre.next;
+            }
+            else {
+                pre.next = cur.next;
+            }
+
+            cur = cur.next;
+        }
+
+        return FakeHead.next;
     }
 
     public static void main(String[] args) {
