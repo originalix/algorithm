2a49a8de40282b0eb818b9d4cb92127388909264
Lix <xiao.liunit@gmail.com>
2017-11-03 06:58:26 +0800

    test print index

--- Java/LeetCode/src/LeetCode/ReverseLinkedListII_92.java
+++ Java/LeetCode/src/LeetCode/ReverseLinkedListII_92.java
@@ -4,4 +4,14 @@ package LeetCode;
  * Created by Lix on 2017/11/3.
  */
 public class ReverseLinkedListII_92 {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        int index = 1;
+        while (head != null) {
+            System.out.println("head value = " + head.val + ", index = " + index);
+            index += 1;
+            head = head.next;
+        }
+
+        return head;
+    }
 }
