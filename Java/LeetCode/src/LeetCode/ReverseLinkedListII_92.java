a56ce9f06fd415fd8f0c110f9ff13c5039d89c23
Lix <xiao.liunit@gmail.com>
2017-11-05 20:27:38 +0800

    finish reverse Between

--- Java/LeetCode/src/LeetCode/ReverseLinkedListII_92.java
+++ Java/LeetCode/src/LeetCode/ReverseLinkedListII_92.java
@@ -1,10 +1,30 @@
 package LeetCode;
 
+import java.sql.Statement;
+
 /**
  * Created by Lix on 2017/11/3.
  */
 public class ReverseLinkedListII_92 {
     public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        if (head == null ) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        for (int i = 0; i < m - 1; i ++) pre = pre.next;
+
+        ListNode start = pre.next;
+        ListNode then = start.next;
+
+        for (int i = 0; i < n - m; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
     }
 
     public static void main(String[] args) {
