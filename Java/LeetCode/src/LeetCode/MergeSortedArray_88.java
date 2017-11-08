b2f4f2aa6eb34887fa75b7c424db0dc4075b1a0a
Lix <xiao.liunit@gmail.com>
2017-11-08 20:09:33 +0800

    delete unuse code

--- Java/LeetCode/src/LeetCode/MergeSortedArray_88.java
+++ Java/LeetCode/src/LeetCode/MergeSortedArray_88.java
@@ -5,29 +5,7 @@ package LeetCode;
  */
 public class MergeSortedArray_88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            System.out.println(nums2[n]);
-            for (int j = index; j < m; j++) {
-                if (nums2[n] > nums1[j] && nums2[n] <= nums1[j+1]) {
-                    insert(nums1, nums2[n], j+1);
-                    index = j+1;
-                    break;
-                }
-                System.out.println(nums1[m]);
-            }
-        }
-    }
 
-    public void insert(int[] nums1, int obj, int index) {
-        int temp = nums1[index];
-        nums1[index] = obj;
-
-        for (int i = index + 1; i <= nums1.length; i++) {
-            int oldObj = nums1[i];
-            nums1[i] = temp;
-            temp = oldObj;
-        }
     }
 
     public static void main(String[] args) {
