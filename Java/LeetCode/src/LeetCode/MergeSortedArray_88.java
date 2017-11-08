02a8a676e5071bae8960844b1932315498969b26
Lix <xiao.liunit@gmail.com>
2017-11-08 19:50:06 +0800

    merge functiong

--- Java/LeetCode/src/LeetCode/MergeSortedArray_88.java
+++ Java/LeetCode/src/LeetCode/MergeSortedArray_88.java
@@ -4,4 +4,12 @@ package LeetCode;
  * Created by Lix on 2017/11/8.
  */
 public class MergeSortedArray_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(nums2[n]);
+            for (int j = 0; j < m; j++) {
+                System.out.println(nums1[m]);
+            }
+        }
+    }
 }
