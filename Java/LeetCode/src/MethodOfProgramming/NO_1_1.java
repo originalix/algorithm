2aab8e92daf26387e11c7c12e6662cca0e8558a0
Lix <xiao.liunit@gmail.com>
2017-12-17 21:17:14 +0800

    test rotate String

--- Java/LeetCode/src/MethodOfProgramming/NO_1_1.java
+++ Java/LeetCode/src/MethodOfProgramming/NO_1_1.java
@@ -9,20 +9,27 @@ package MethodOfProgramming;
  * 来自章节1.1.1
  */
 public class NO_1_1 {
-    public static void LeftRotateString(String[] s, int n, int m)
-    {
+    public static void LeftRotateString(String[] s, int n, int m) {
         while (m > 0) {
             LeftShiftOne(s, n);
             m -= 1;
         }
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
     }
 
-    private static void LeftShiftOne(String[] s, int n)
-    {
+    private static void LeftShiftOne(String[] s, int n) {
         String t = s[0];
         for (int i = 1; i < n; i++) {
             s[i - 1] = s[i];
         }
         s[n - 1] = t;
+    }
+
+    public static void main(String[] args) {
+        String[] s = new String[]{"a", "b", "c", "d", "e", "f", "g"};
+        LeftRotateString(s, 7, 3);
     }
 }
