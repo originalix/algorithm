340e9a1fcb13471a0ebb177261cc582a8799ee33
Lix <xiao.liunit@gmail.com>
2017-12-17 21:06:03 +0800

    create leftRoteString

--- Java/LeetCode/src/MethodOfProgramming/NO_1_1.java
+++ Java/LeetCode/src/MethodOfProgramming/NO_1_1.java
@@ -9,4 +9,20 @@ package MethodOfProgramming;
  * 来自章节1.1.1
  */
 public class NO_1_1 {
+    public static void LeftRotateString(String[] s, int n, int m)
+    {
+        while (m > 0) {
+            LeftShiftOne(s, n);
+            m -= 1;
+        }
+    }
+
+    private static void LeftShiftOne(String[] s, int n)
+    {
+        String t = s[0];
+        for (int i = 1; i < n; i++) {
+            s[i - 1] = s[i];
+        }
+        s[n - 1] = t;
+    }
 }
