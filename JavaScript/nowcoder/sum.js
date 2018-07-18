c5b0acc17dbb76556d6ea459e4aa49e8f1e639e3
Lix <xiao.liunit@gmail.com>
2018-07-18 12:02:35 +0800

    [多种方式求和]

--- JavaScript/nowcoder/sum.js
+++ JavaScript/nowcoder/sum.js
@@ -2,6 +2,7 @@
  * 计算给定数组 arr 中所有元素的总和
  * @param {*} arr
  */
+// 常规循环完成
 function sum(arr) {
   var sum = 0;
   for (var i = 0; i < arr.length; i++) {
@@ -10,6 +11,7 @@ function sum(arr) {
   return sum;
 }
 
+// 递归完成
 function sumRecursion(arr) {
   var len = arr.length;
   if (len === 0) {
@@ -21,14 +23,31 @@ function sumRecursion(arr) {
   }
 }
 
+// 函数式编程 map-reduce
 function sumMapReduce(arr) {
   return arr.reduce(function(prev, curr, idx, arr) {
     return prev + curr;
-  })
+  });
+}
+
+// forEach遍历
+function sumForEach(arr) {
+  var sum = 0;
+  arr.forEach(function(curr, idx, arr) {
+    sum += curr;
+  }, 0);
+  return sum;
+}
+
+// eval完成
+function sumEval(arr) {
+  return eval(arr.join('+'));
 }
 
 var arr = [1, 2, 3, 4];
 // var sum = sum(arr);
 // var sum = sumRecursion(arr);
-var sum = sumMapReduce()
+// var sum = sumMapReduce(arr);
+// var sum = sumForEach(arr);
+var sum = sumEval(arr);
 console.log(sum);
