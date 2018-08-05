9129eb7411122c275b465255b78290c9bf15ec76
Lix <xiao.liunit@gmail.com>
2018-08-05 09:47:47 +0800

    [选择排序第一版]

--- JavaScript/algorithms/sorting/selection-sort/SelectionSort.js
+++ JavaScript/algorithms/sorting/selection-sort/SelectionSort.js
@@ -3,7 +3,26 @@ class SelectionSort {
     const array = [...originalArray];
 
     for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
 
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex != i) {
+        let temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+      }
     }
+
+    return array;
   }
 }
+
+let arr = [1, 6, 4, 5, 3, 9 , 0];
+let selection = new SelectionSort();
+let res = selection.sort(arr);
+console.log(res);
