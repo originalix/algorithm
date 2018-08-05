834a06f41a37113a2f0b3ad75d2a4687e2538712
Lix <xiao.liunit@gmail.com>
2018-08-05 09:58:24 +0800

    [更新插入排序]

--- JavaScript/algorithms/sorting/selection-sort/SelectionSort.js
+++ JavaScript/algorithms/sorting/selection-sort/SelectionSort.js
@@ -1,18 +1,29 @@
+/**
+ *  选择排序算法
+ *  最优时间复杂度 n^2
+ *  平均时间复杂度 n^2
+ *  最差时间复杂度 n^2
+ *  占用内存 1
+ *  是否稳定 否
+ */
 class SelectionSort {
   sort(originalArray) {
+    // 克隆数组 不在原数组上修改
     const array = [...originalArray];
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
       let minIndex = i;
 
+      // 在数组的剩余部分中 寻找最小部分
       for (let j = i + 1; j < array.length; j++) {
         if (array[j] < array[minIndex]) {
           minIndex = j;
         }
       }
 
-      if (minIndex != i) {
-        let temp = array[i];
+      // 如果最小的数被查找到，那就与原来的minIndex做一次交换
+      if (minIndex !== i) {
+        const temp = array[i];
         array[i] = array[minIndex];
         array[minIndex] = temp;
       }
