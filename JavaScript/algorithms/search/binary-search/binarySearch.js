24af3ab7d3ecc58149f1941cf05f758ae782bad0
Lix <xiao.liunit@gmail.com>
2018-09-29 20:54:18 +0800

    [测试二分查找]

--- JavaScript/algorithms/search/binary-search/binarySearch.js
+++ JavaScript/algorithms/search/binary-search/binarySearch.js
@@ -10,15 +10,34 @@ class BinarySearch {
     const middle = parseInt((low + high) / 2);
     if (this.array[middle] > target) {
       return this.search(low, middle - 1, target);
-    } else if (this.array[middle] < target) {
-      return this.search(middle + 1, high, target);
-    } else {
-      return middle;
     }
+    if (this.array[middle] < target) {
+      return this.search(middle + 1, high, target);
+    }
+    return middle;
   }
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const binary = new BinarySearch(array);
-const result = binary.search(0, array.length - 1, 0);
-console.log(result);
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const binary = new BinarySearch(array);
+// const result = binary.search(0, array.length - 1, 0);
+// console.log(result);
+
+Array.prototype.binary_search = function(low, high, target) {
+  if (low > high) {
+    console.log(`low = ${low}, high = ${high}`);
+    return -1;
+  }
+
+  const middle = parseInt((low + high) / 2);
+  if (this[middle] > target) {
+    return this.binary_search(low, middle - 1, target);
+  }
+  if (this[middle] < target) {
+    return this.binary_search(middle + 1, high, target);
+  }
+  return middle;
+}
+
+const res = array.binary_search(0, array.length - 1, 0);
+console.log(res);
