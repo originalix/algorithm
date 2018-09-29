79a717c176875ce51f57ba6f8ede64b71ffa8f10
Lix <xiao.liunit@gmail.com>
2018-09-29 21:04:48 +0800

    [更新二分搜索]

--- JavaScript/algorithms/search/binary-search/binarySearch.js
+++ JavaScript/algorithms/search/binary-search/binarySearch.js
@@ -1,3 +1,8 @@
+/**
+ * 二分搜索法
+ * 对有序数组进行搜索
+ * 时间复杂度 O(log(n))
+ */
 class BinarySearch {
   constructor(originalArray) {
     this.array = originalArray;
@@ -20,24 +25,20 @@ class BinarySearch {
 
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // const binary = new BinarySearch(array);
-// const result = binary.search(0, array.length - 1, 0);
+// const result = binary.search(0, array.length - 1, 8);
 // console.log(result);
 
-Array.prototype.binary_search = function(low, high, target) {
-  if (low > high) {
-    console.log(`low = ${low}, high = ${high}`);
+Array.prototype.binarySearch = function(low, high, target) {
+  if (low > high)
     return -1;
-  }
 
   const middle = parseInt((low + high) / 2);
-  if (this[middle] > target) {
-    return this.binary_search(low, middle - 1, target);
-  }
-  if (this[middle] < target) {
-    return this.binary_search(middle + 1, high, target);
-  }
+  if (this[middle] > target)
+    return this.binarySearch(low, middle - 1, target);
+  if (this[middle] < target)
+    return this.binarySearch(middle + 1, high, target);
   return middle;
 }
 
-const res = array.binary_search(0, array.length - 1, 0);
-console.log(res);
+const result = array.binarySearch(0, array.length - 1, 5);
+console.log(result);
