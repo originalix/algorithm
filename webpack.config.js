c621957382882335997bdd4ce30ff4f2fd1950e2
Lix <lixiao.dev@gmail.com>
2021-02-01 20:01:56 +0800

    feat: webpack 多入口配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
+const fs = require('fs')
 const path = require('path')
 const ESlintPlugin = require('eslint-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+const entrys = {}
+const algs4Path = path.resolve(__dirname, 'src/algs4')
+
+function readFileDir(filePath) {
+  const files = fs.readdirSync(filePath)
+  for (const fileName of files) {
+    const fileDir = path.join(filePath, fileName)
+    const stats = fs.statSync(fileDir)
+    if (stats.isFile()) {
+      if (/\.[tj]s$/.test(fileName)) {
+        const key = fileName.match(/(^\w{1,})(?=\.[tj]s)/ig)
+        key && key.length && (entrys[key[0]] = fileDir)
+      }
+    }
+    if (stats.isDirectory()) {
+      readFileDir(fileDir)
+    }
+  }
+}
+readFileDir(algs4Path)
+
 module.exports = {
+  target: 'node',
   mode: 'development',
   entry: {
-    index: './src/index.ts'
+    index: './src/index.ts',
+    ...entrys
   },
   module: {
     rules: [
