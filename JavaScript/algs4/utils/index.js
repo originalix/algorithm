dbb290ef355e3ba9352754ed1a0276d4d7b01725
Lix <lixiao.dev@gmail.com>
2021-02-01 15:34:18 +0800

    refactor: remove all file

--- JavaScript/algs4/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var stop_watch_1 = require("./stop-watch");
-exports.StopWatch = stop_watch_1["default"];
-exports.readInt = function (max) {
-    return Math.floor(Math.random() * Math.floor(max));
-};
--- JavaScript/algs4/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import StopWatch from './stop-watch'
-
-export const readInt = (max: number): number => {
-  return Math.floor(Math.random() * Math.floor(max))
-}
-
-export { StopWatch }
