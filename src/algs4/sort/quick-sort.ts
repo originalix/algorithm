4898b9535cad21a20b5f6cd4a6bea398acd9a7c9
Lix <lixiao.dev@gmail.com>
2021-02-04 07:29:27 +0800

    feat: 优化快速排序

--- src/algs4/sort/quick-sort.ts
+++ src/algs4/sort/quick-sort.ts
@@ -3,33 +3,28 @@ import { StdIn, StopWatch } from 'utils'
 import { SortMockFile } from '@/constants'
 
 class QuickSort<T> extends BaseSort<T> {
-  sort() {
-    this.array = this._sort(this.array)
+  sort(arr: T[]) {
+    this._sort(arr, 0, arr.length - 1)
   }
 
-  private _sort(originArr: T[]): T[] {
-    const array = [...originArr]
-    if (array.length <= 1) return array
+  private _sort(arr: T[], lo: number, hi: number) {
+    if (hi <= lo) return
+    const j = this.partition(arr, lo, hi)
+    this._sort(arr, lo, j - 1)
+    this._sort(arr, j + 1, hi)
+  }
 
-    const pivotElement = array.shift()
-    const centerArr = []
-
-    const leftArr = []
-    const rightArr = []
-
-    while (array.length) {
-      const currentElement = array.shift()
-      if (currentElement < pivotElement) {
-        leftArr.push(currentElement)
-      } else if (currentElement > pivotElement) {
-        rightArr.push(currentElement)
-      } else {
-        centerArr.push(currentElement)
-      }
+  private partition(arr: T[], lo: number, hi: number): number {
+    let i = lo; let j = hi + 1
+    const v = arr[lo]
+    while (true) {
+      while (this.less(arr[++i], v)) if (i === hi) break
+      while (this.less(v, arr[--j])) if (j === lo) break
+      if (i >= j) break
+      this.exch(i, j)
     }
-    const leftSortedArr = this._sort(leftArr)
-    const rightSortedArr = this._sort(rightArr)
-    return leftSortedArr.concat(centerArr, rightSortedArr)
+    this.exch(lo, j)
+    return j
   }
 }
 
@@ -37,7 +32,7 @@ async function main() {
   const data = await StdIn.readInt(SortMockFile)
   const stopWatch = new StopWatch()
   const quick = new QuickSort<number>(data)
-  quick.main()
+  quick.main(true)
   stopWatch.elapseTime()
 }
 
