f6852756aaf55ac5e9f6ba7b15925c70bf8bcc66
Lix <lixiao.dev@gmail.com>
2021-02-24 21:22:00 +0800

    feat: 顺序查找 基于无序链表

--- src/algs4/search/sequential-search-st.ts
+++ src/algs4/search/sequential-search-st.ts
@@ -1,3 +1,4 @@
+// 链表结点的定义
 export class Node<Key, Value> {
   public key: Key
   public val: Value
@@ -9,8 +10,30 @@ export class Node<Key, Value> {
   }
 }
 
+/**
+ * 顺序查找（基于无序链表）
+ */
 class SequentialSearchST<Key, Value> {
-  private first: Node<Key, Value>
+  private first: Node<Key, Value> // 链表首结点
+  private N: number
+
+  constructor() {
+    this.first = null
+    this.N = 0
+  }
+
+  size(): number { return this.N }
+
+  keys(): Key[] {
+    const keys = []
+    while (this.first !== null) {
+      keys.push(this.first.key)
+      this.first = this.first.next
+    }
+    return keys
+  }
+
+  // 查找给定的键，返回相关联的值
   get(key: Key): Value {
     for (let x = this.first; x !== null; x = x.next) {
       if (key === x.key) return x.val
@@ -18,6 +41,7 @@ class SequentialSearchST<Key, Value> {
     return null
   }
 
+  // 查找给定的键，找到则更新其值，否则在表中新建节点
   put(key: Key, val: Value) {
     for (let x = this.first; x !== null; x = x.next) {
       if (key === x.key) {
@@ -26,6 +50,22 @@ class SequentialSearchST<Key, Value> {
       }
     }
     this.first = new Node<Key, Value>(key, val, this.first)
+    this.N++
+  }
+
+  delete(key: Key) {
+    if (key === null) throw new Error('argument to delete() is null')
+    return this._delete(this.first, key)
+  }
+
+  private _delete(node: Node<Key, Value>, key: Key) {
+    if (node === null) return null
+    if (key === node.key) {
+      this.N--
+      return node.next
+    }
+    node.next = this._delete(node.next, key)
+    return node
   }
 }
 
@@ -33,9 +73,11 @@ function main() {
   const words = 'helloworld'.split('')
   const st = new SequentialSearchST<string, number>()
   words.forEach((v, i) => {
-    st.put(v, i)
+    st.put(v, i + 1)
   })
-  console.log(st)
+  st.delete('o')
+  const keys = st.keys()
+  console.log(keys)
 }
 
 main()
