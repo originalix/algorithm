507fde515022268325417873f9abb712a6058f72
Lix <lixiao.dev@gmail.com>
2021-03-14 20:52:22 +0800

    feat: 背包迭代

--- src/algs4/1-3/bag.ts
+++ src/algs4/1-3/bag.ts
@@ -1,4 +1,4 @@
-import { NodeItem } from '@/types'
+import { NodeItem, NodeIterator } from '@/types'
 
 /**
  * 数据结构 背包
@@ -7,11 +7,12 @@ import { NodeItem } from '@/types'
  * @class Bag
  * @template T
  */
-export default class Bag<T> {
+export default class Bag<T> extends NodeIterator<T> {
   private first: NodeItem<T>
   private N: number
 
   constructor() {
+    super()
     this.first = null
     this.N = 0
   }
@@ -30,5 +31,22 @@ export default class Bag<T> {
     this.first.item = item
     this.first.next = oldFirst
     this.N++
+    this.setCurrent(this.first)
   }
 }
+
+function main() {
+  const val = [1, 3, 3, 4, 5, 6, 7, 8, 9, 4, 3, 5234, 234, 234, 234, 234, 234, 234]
+  const bag = new Bag<number>()
+  for (const v of val) {
+    bag.add(v)
+  }
+  console.log(`背包内元素数量: ${bag.size()}`)
+
+  while (bag.hasNext()) {
+    const element = bag.next()
+    console.log(`元素遍历: ${element}`)
+  }
+}
+
+main()
