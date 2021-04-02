8d4b1e2fdc2f967ff60a76b87e708d327798a2dd
Lix <lixiao.dev@gmail.com>
2021-04-02 20:41:13 +0800

    feat: use jest for unit test lib

---
+++ __test__/evaluate.spec.ts
@@ -0,0 +1,10 @@
+import { strictEqual } from 'assert'
+import Evaluate from '../src/algs4/1-3/evaluate'
+
+describe('Dijkstra 的双栈算术表达式求值算法', () => {
+  test('(1 + ((2 + 3) * (4 * 5))) = 101', () => {
+    const formula = '(1 + ((2 + 3) * (4 * 5)))'
+    const val = Evaluate.main(formula)
+    strictEqual(+val, 101)
+  })
+})
