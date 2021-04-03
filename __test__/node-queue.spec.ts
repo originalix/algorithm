892febca8215c9e124a2cd9478e10b35f91fe2c1
Lix <lixiao.dev@gmail.com>
2021-04-03 20:58:11 +0800

    test: add stack unit test

--- __test__/node-queue.spec.ts
+++ __test__/node-queue.spec.ts
@@ -3,15 +3,10 @@ import { TinyIntFile } from '../src/constants'
 import Queue from '../src/algs4/1-3/node-queue'
 
 describe('Queue 队列测试', () => {
-  const time = new StopWatch()
-  let val: number[]
-  let queue: Queue<number> | null
-  beforeAll(async () => {
-    val = await StdIn.readInt(TinyIntFile)
-    queue = new Queue<number>()
-  })
-
-  test('queue enqueue and dequeue', () => {
+  test('queue enqueue and dequeue', async () => {
+    const time = new StopWatch()
+    const val = await StdIn.readInt(TinyIntFile)
+    const queue = new Queue<number>()
     for (const v of val) {
       queue.enqueue(v)
     }
@@ -20,9 +15,8 @@ describe('Queue 队列测试', () => {
     while (!queue.isEmpty()) {
       queue.dequeue()
     }
+    time.elapseTime()
 
     expect(queue.size()).toBe(0)
-
-    time.elapseTime()
   })
 })
---
+++ __test__/node-stack.spec.ts
@@ -0,0 +1,23 @@
+import { StdIn, StopWatch } from '../src/utils'
+import { TinyIntFile } from '../src/constants'
+import Stack from '../src/algs4/1-3/node-stack'
+
+describe('Stack 下压栈测试', () => {
+  test('stack push and pop', async () => {
+    const time = new StopWatch()
+    const val = await StdIn.readInt(TinyIntFile)
+    const stack = new Stack<number>()
+    for (const v of val) {
+      stack.push(v)
+    }
+
+    expect(stack.size()).toBe(val.length)
+
+    while (!stack.isEmpty()) {
+      stack.pop()
+    }
+    time.elapseTime()
+
+    expect(stack.size()).toBe(0)
+  })
+})
