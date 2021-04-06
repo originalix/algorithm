42a4dc9f89a8c3c77e0e1c830255be4a723a3ef7
Lix <lixiao.dev@gmail.com>
2021-04-06 11:48:06 +0800

    style: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,28 +6,32 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   globals: { ...jestMap },
   extends: [
-    'standard',
     'eslint:recommended',
-    'plugin:node/recommended'
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always'
-    }],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
     'node/no-unsupported-features/es-syntax': 'off',
-    'node/no-missing-import': 'off'
-  }
+    'node/no-missing-import': 'off',
+  },
 }
